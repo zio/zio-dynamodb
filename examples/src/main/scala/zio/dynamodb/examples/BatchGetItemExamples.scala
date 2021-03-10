bc5348d8084075484900f0426f825e48089532a0
Avinder Bahra <avinder.bahra@gmail.com>
2021-03-10 09:37:46 +0000

    fix compile errors

--- examples/src/main/scala/zio/dynamodb/examples/BatchGetItemExamples.scala
+++ examples/src/main/scala/zio/dynamodb/examples/BatchGetItemExamples.scala
@@ -1,11 +1,13 @@
 package zio.dynamodb.examples
 
 import zio.dynamodb.DynamoDBQuery.BatchGetItem
-import zio.dynamodb.{ AttributeValue, PrimaryKey, TableName }
+import zio.dynamodb.{ AttributeValue, MapOfSet, PrimaryKey, TableName }
 
 class BatchGetItemExamples {
   val pk1    = PrimaryKey(Map("field1" -> AttributeValue.Number(1.0)))
   val table1 = TableName("T1")
   val table2 = TableName("T2")
-  val batch  = BatchGetItem(Map(table1 -> BatchGetItem.TableItem(pk1), table2 -> BatchGetItem.TableItem(pk1)))
+  val batch  = BatchGetItem(
+    MapOfSet(Map(table1 -> Set(BatchGetItem.TableItem(pk1)), table2 -> Set(BatchGetItem.TableItem(pk1))))
+  )
 }
