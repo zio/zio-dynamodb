5ef691b489f8d8ac4eb8f6d3c61d55482a43643b
Avinder Bahra <avinder.bahra@gmail.com>
2021-05-05 17:55:43 +0100

    draft PE parser working

--- dynamodb/src/main/scala/zio/dynamodb/Parser.scala
+++ dynamodb/src/main/scala/zio/dynamodb/Parser.scala
@@ -1,5 +1,6 @@
 package zio.dynamodb
 
+import zio.Chunk
 import zio.dynamodb.ProjectionExpression.Root
 
 object Parser extends App {
@@ -37,6 +38,52 @@ $("foo.bar[9].baz")
     def getPE: Option[ProjectionExpression] = pe
   }
 
+  case class PEBuilder2(pe: Option[ProjectionExpression] = None) { self =>
+
+    def addChildMap(s: String): PEBuilder2 =
+      PEBuilder2(pe.fold[Option[ProjectionExpression]](Some(Root(s)))(pe => Some(pe(s))))
+
+    def addChildArray(s: String, i: Int): PEBuilder2 =
+      PEBuilder2(pe.fold[Option[ProjectionExpression]](Some(Root(s)))(pe => Some(pe(s))).map(_(i)))
+
+    // TODO: remove
+    def getPE: Option[ProjectionExpression] = pe
+  }
+
+  case class PEBuilder3(pe: Option[Either[Chunk[String], ProjectionExpression]] = None) { self =>
+
+    def addChildMap(s: String): PEBuilder3 =
+      PEBuilder3(pe match {
+        case None                     =>
+          Some(Right(Root(s)))
+        case Some(Right(pe))          =>
+          Some(Right(pe(s)))
+        case someLeft @ Some(Left(_)) =>
+          someLeft
+      })
+
+    def addChildArray(s: String, i: Int): PEBuilder3 =
+      PEBuilder3(pe match {
+        case None                     =>
+          Some(Right(Root(s)))
+        case Some(Right(pe))          =>
+          Some(Right(pe(s)(i)))
+        case someLeft @ Some(Left(_)) =>
+          someLeft
+      })
+
+    def addError(s: String): PEBuilder3 =
+      PEBuilder3(pe match {
+        case None | Some(Right(_)) =>
+          Some(Left(Chunk(s"error with $s")))
+        case Some(Left(chunk))     =>
+          Some(Left(chunk :+ s"error with $s"))
+      })
+
+    // TODO: remove
+    def getPE: Option[Either[Chunk[String], ProjectionExpression]] = pe
+  }
+
   val regexIndex = """(^[a-zA-Z_]+)\[([0-9]+)]""".r
   val regexMap   = """(^[a-zA-Z_]+)""".r
 
@@ -53,7 +100,7 @@ $("foo.bar[9].baz")
           case regexMap(name)          =>
             pe.addChildMap(name)
           case _                       =>
-            println("No match! TODO: error!")
+            println("XXXXXXXXXXXXX No match! TODO: error!")
             pe
         }
     }
@@ -61,7 +108,30 @@ $("foo.bar[9].baz")
     pe.getPE
   }
 
-  val x = parse("bar_Foo.baz[9]")
+  def parse2(s: String): Either[Chunk[String], ProjectionExpression] = {
+
+    val elements: List[String] = s.split("\\.").toList
+    elements.foreach(println)
+
+    val pe = elements.foldLeft(PEBuilder3()) {
+      case (pe, s) =>
+        s match {
+          case regexIndex(name, index) =>
+            pe.addChildArray(name, index.toInt)
+          case regexMap(name)          =>
+            pe.addChildMap(name)
+          case _                       =>
+            pe.addError(s)
+        }
+    }
+
+    val value: Either[Chunk[String], ProjectionExpression] = pe.getPE.getOrElse(Left(Chunk("Empty ProjectExpression")))
+    value
+  }
+
+  val x  = parse("bar_$Foo.baz[9]")
   println(x)
+  val x2 = parse2("$barFoo.baz[9].boom.b$umba")
+  println(x2)
 
 }
