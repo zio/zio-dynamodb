14c2a0602d84266f1ccb46b47e0021def37d3432
Avinder Bahra <avinder.bahra@gmail.com>
2021-05-12 06:42:50 +0100

    playing around with generators

--- dynamodb/src/test/scala/zio/dynamodb/ProjectionExpressionParserSpec.scala
+++ dynamodb/src/test/scala/zio/dynamodb/ProjectionExpressionParserSpec.scala
@@ -1,12 +1,54 @@
 package zio.dynamodb
 
 import zio.dynamodb.ProjectionExpression.{ parse, ListElement, MapElement, Root }
-import zio.test.Assertion.{ equalTo, isLeft, isRight }
+import zio.random.Random
+import zio.test.Assertion._
 import zio.test.{ DefaultRunnableSpec, _ }
 
 object ProjectionExpressionParserSpec extends DefaultRunnableSpec {
+  object Generators {
+    def fieldName                                                                                    = Gen.anyString
+    def index                                                                                        = Gen.anyInt
+    def root: Gen[Random with Sized, Root]                                                           = fieldName.map(Root)
+    def mapElement(parent: => ProjectionExpression)                                                  = fieldName.map(MapElement(parent, _))
+    def listElement(parent: => ProjectionExpression)                                                 = index.map(ListElement(parent, _))
+    def mapOrListElement(parent: ProjectionExpression): Gen[Random with Sized, ProjectionExpression] =
+      Gen.oneOf(mapElement(parent))
+
+    def projectionExpression: Gen[Random with Sized, ProjectionExpression] = {
+      def loop(
+        parentGen: Gen[Random with Sized, ProjectionExpression],
+        counter: Int
+      ): Gen[Random with Sized, ProjectionExpression] =
+        if (counter == 0)
+          parentGen
+        else
+          loop(parentGen.flatMap(pe => mapOrListElement(pe)), counter - 1)
+
+      for {
+        count <- Gen.int(1, 20)
+        pe    <- loop(root, count)
+      } yield pe
+    }
+
+  }
+
   override def spec: ZSpec[Environment, Failure] =
     suite("ProjectionExpression Parser")(
+      // TODO
+      testM("should report syntax errors with non empty invalid ProjectionExpressions") {
+        check(Generators.projectionExpression) { pe =>
+          assert(
+            parse(pe.toString)
+          ) {
+            isLeft(
+              startsWithString(
+                "error with"
+              )
+            )
+          }
+        }
+      },
       test("toString on a ProjectionExpression of foo.bar[9].baz") {
         val pe = MapElement(ListElement(MapElement(Root("foo"), "bar"), 9), "baz")
         assert(pe.toString)(equalTo("foo.bar[9].baz"))
