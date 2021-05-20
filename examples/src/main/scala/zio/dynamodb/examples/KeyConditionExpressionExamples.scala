52343c0e9b78c5d36897874140d3b44d8ff8f37b
Avinder Bahra <avinder.bahra@gmail.com>
2021-05-20 07:16:05 +0100

    improved examples a little bit more

--- examples/src/main/scala/zio/dynamodb/examples/KeyConditionExpressionExamples.scala
+++ examples/src/main/scala/zio/dynamodb/examples/KeyConditionExpressionExamples.scala
@@ -6,19 +6,10 @@ import zio.dynamodb._
 
 object KeyConditionExpressionExamples extends App {
 
-//  // TODO: KeyConditionExpression should allow just a PartitionKey
-//  val exprnPKOnly: PartitionKeyExpression = PartitionKey("partitionKey1")
+  val exprn1: KeyConditionExpression = PartitionKey("partitionKey1") === "x"
 
-  val exprn: KeyConditionExpression =
-    PartitionKey("partitionKey1") === "x" &&
-      SortKey("sortKey1") > "X"
+  val exprn2: KeyConditionExpression = PartitionKey("partitionKey1") === "x" && SortKey("sortKey1") > "X"
 
-  val exprn2: KeyConditionExpression =
-    PartitionKey("partitionKey1") === "x" &&
-      SortKey("sortKey1") === "X"
-
-  val exprn3: KeyConditionExpression = PartitionKey("partitionKey1") === "x"
-
-  println(exprn)
+  val exprn3: KeyConditionExpression = PartitionKey("partitionKey1") === "x" && SortKey("sortKey1") === "X"
 
 }
