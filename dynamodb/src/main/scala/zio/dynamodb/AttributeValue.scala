eb28d9f159be4e5a803f58a59781393b205ba29a
Avinder Bahra <avinder.bahra@gmail.com>
2021-05-22 08:15:56 +0100

    moved ToAttributeValue to own file

--- dynamodb/src/main/scala/zio/dynamodb/AttributeValue.scala
+++ dynamodb/src/main/scala/zio/dynamodb/AttributeValue.scala
@@ -51,35 +51,6 @@ object AttributeValue {
 
 }
 
-// TODO move to own file
-trait ToAttributeValue[-A] {
-  def toAttributeValue(a: A): AttributeValue
-}
-object ToAttributeValue extends ToAttributeValueLowPriorityImplicits {
-  import Predef.{ String => ScalaString }
-  import Predef.{ Map => ScalaMap }
-
-  implicit val binaryToAttributeValue: ToAttributeValue[Iterable[Byte]]              = AttributeValue.Binary(_)
-  implicit val binarySetToAttributeValue: ToAttributeValue[Iterable[Iterable[Byte]]] = AttributeValue.BinarySet(_)
-  implicit val boolToAttributeValue: ToAttributeValue[Boolean]                       = AttributeValue.Bool(_)
-
-  implicit def mapToAttributeValue[A](implicit
-    element: ToAttributeValue[A]
-  ): ToAttributeValue[ScalaMap[ScalaString, A]] =
-    (map: ScalaMap[ScalaString, A]) =>
-      AttributeValue.Map {
-        map.map {
-          case (key, value) => (AttributeValue.String(key), element.toAttributeValue(value))
-        }
-      }
-
-  implicit val stringToAttributeValue: ToAttributeValue[String]              = AttributeValue.String(_)
-  implicit val stringSetToAttributeValue: ToAttributeValue[Set[ScalaString]] =
-    AttributeValue.StringSet(_)
-  implicit val numberToAttributeValue: ToAttributeValue[BigDecimal]          = AttributeValue.Number(_)
-  implicit val numberSetToAttributeValue: ToAttributeValue[Set[BigDecimal]]  = AttributeValue.NumberSet(_)
-}
-
 trait ToAttributeValueLowPriorityImplicits {
   implicit def listToAttributeValue[A](implicit element: ToAttributeValue[A]): ToAttributeValue[Iterable[A]] =
     (xs: Iterable[A]) => AttributeValue.List(Chunk.fromIterable(xs.map(element.toAttributeValue)))
