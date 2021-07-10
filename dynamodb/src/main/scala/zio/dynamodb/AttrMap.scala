69f163434cd82d5c43dfa08414951a7ba440763b
Avinder Bahra <avinder.bahra@gmail.com>
2021-07-10 13:24:45 +0100

    deleted dead code

--- dynamodb/src/main/scala/zio/dynamodb/AttrMap.scala
+++ dynamodb/src/main/scala/zio/dynamodb/AttrMap.scala
@@ -1,12 +1,10 @@
 package zio.dynamodb
 
 final case class AttrMap(map: Map[String, AttributeValue]) {
-  def get[A](field: String)(implicit ev: FromAttributeValue[A]): Option[A]            =
+
+  def get[A](field: String)(implicit ev: FromAttributeValue[A]): Option[A] =
     map.get(field).map(ev.fromAttributeValue).flatten
-//  map.get(field) match {
-//    case Some(AttributeValue.String(s)) => Some(s).asInstanceOf[Option[A]]
-//    case Some(AttributeValue.Number(i)) => Some(i).asInstanceOf[Option[A]]
-//  }
+
   def getOpt[A](field: String)(implicit ev: FromAttributeValue[A]): Option[Option[A]] =
     map.get(field).map(ev.fromAttributeValue)
 
