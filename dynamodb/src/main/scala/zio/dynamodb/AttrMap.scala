dc033f324264e282721a8bcf5752974821a1c44a
Avinder Bahra <avinder.bahra@gmail.com>
2021-07-11 17:45:36 +0100

    playing with getOpt

--- dynamodb/src/main/scala/zio/dynamodb/AttrMap.scala
+++ dynamodb/src/main/scala/zio/dynamodb/AttrMap.scala
@@ -2,12 +2,16 @@ package zio.dynamodb
 
 final case class AttrMap(map: Map[String, AttributeValue]) {
 
-  def get[A](field: String)(implicit ev: FromAttributeValue[A]): Option[A] =
+  def get[A](field: String)(implicit ev: FromAttributeValue[A]): Either[String, A] =
+    map.get(field).map(ev.fromAttributeValue).flatten.toRight(s"field '$field' not found")
+
+  def getOpt[A](field: String)(implicit ev: FromAttributeValue[A]): Option[A] =
     map.get(field).map(ev.fromAttributeValue).flatten
 
-  def getOpt[A](field: String)(implicit ev: FromAttributeValue[A]): Option[Option[A]] =
-    map.get(field).map(ev.fromAttributeValue)
-
+//  def getOptAttrMap[A, B](
+//    field: String
+//  )(f: AttrMap => Either[String, Option[B]])(implicit ev: FromAttributeValue[A]): Option[A] =
+//    getOpt[A](field).fold[Either[String, Option[B]]](Right(None))(f)
 }
 
 object AttrMap extends GeneratedAttrMapApplies {
