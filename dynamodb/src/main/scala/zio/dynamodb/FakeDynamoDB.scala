cf99c905bca3fbe7c11197eb0df134a8aab83a9c
Avinder Bahra <avinder.bahra@gmail.com>
2021-07-21 21:13:34 +0100

    draft implementation of scanSome in Database

--- dynamodb/src/main/scala/zio/dynamodb/FakeDynamoDB.scala
+++ dynamodb/src/main/scala/zio/dynamodb/FakeDynamoDB.scala
@@ -1,9 +1,10 @@
 package zio.dynamodb
 
 import zio.dynamodb.DynamoDBExecutor.DynamoDBExecutor
+import zio.dynamodb.DynamoDBExecutor.TestData.emptyItem
 import zio.dynamodb.DynamoDBQuery.BatchGetItem.TableGet
-import zio.dynamodb.DynamoDBQuery.{ BatchGetItem, BatchWriteItem, DeleteItem, GetItem, PutItem, UpdateItem }
-import zio.{ Ref, UIO, ULayer, ZIO }
+import zio.dynamodb.DynamoDBQuery.{ BatchGetItem, BatchWriteItem, DeleteItem, GetItem, PutItem, QuerySome, UpdateItem }
+import zio.{ Chunk, Ref, UIO, ULayer, ZIO }
 
 case class Database(
   map: Map[String, Map[PrimaryKey, Item]] = Map.empty,
@@ -28,6 +29,71 @@ case class Database(
   // TODO: consider returning just Database
   def remove(tableName: String, pk: PrimaryKey): Option[Database] =
     self.map.get(tableName).map(m => Database(self.map + (tableName -> (m - pk)), self.tablePkMap))
+
+  def scanSome(tableName: String, exclusiveStartKey: LastEvaluatedKey, limit: Int): (Chunk[Item], LastEvaluatedKey) = {
+    val items: (Chunk[Item], LastEvaluatedKey) = (for {
+      itemMap <- self.map.get(tableName)
+      pkName  <- tablePkMap.get(tableName)
+      xs      <- Some(range(sort(itemMap.toList, pkName), exclusiveStartKey, limit))
+    } yield xs).getOrElse((Chunk.empty, None))
+    items
+  }
+
+  private def sort(xs: Seq[(PrimaryKey, Item)], pkName: String): Seq[(PrimaryKey, Item)] =
+    xs.toList.sortWith {
+      case ((pkL, _), (pkR, _)) =>
+        (pkL.map.get(pkName), pkR.map.get(pkName)) match {
+          case (Some(left), Some(right)) => attributeValueOrdering(left, right)
+          case _                         => false
+        }
+    }
+
+  private def range(
+    xs: Seq[(PrimaryKey, Item)],
+    exclusiveStartKey: LastEvaluatedKey,
+    limit: Int
+  ): (Chunk[Item], LastEvaluatedKey) = {
+    val index = nextIndex(xs, exclusiveStartKey)
+    if (index > -1) {
+      val subset         = xs.slice(index, index + limit)
+      val x: Chunk[Item] = Chunk.fromIterable(subset.map(_._2))
+      (x, Some(subset.last._1))
+    } else
+      (Chunk.empty, None)
+  }
+
+  // TODO: signal last record read somehow, and feed that back as a None upstream
+  // returns -1 if is exclusiveStartKey is not found, else next index
+  private def nextIndex(xs: Seq[(PrimaryKey, Item)], exclusiveStartKey: LastEvaluatedKey): Int =
+    exclusiveStartKey.fold(0) { pk =>
+      val foundIndex      = xs.indexWhere { case (pk2, _) => pk2 == pk }
+      val afterFoundIndex =
+        if (foundIndex == -1) -1
+        else math.min(foundIndex + 1, xs.length)
+      afterFoundIndex
+    }
+
+  // TODO: I lost the will to live here. TODO come up with a correct ordering scheme
+  private def attributeValueOrdering(left: AttributeValue, right: AttributeValue): Boolean =
+    (left, right) match {
+      case (AttributeValue.Binary(valueL), AttributeValue.Binary(valueR))       =>
+        valueL.toString.compareTo(valueR.toString) < 0
+      case (AttributeValue.Bool(valueL), AttributeValue.Bool(valueR))           =>
+        valueL.compareTo(valueR) < 0
+      case (AttributeValue.List(valueL), AttributeValue.List(valueR))           =>
+        valueL.toString.compareTo(valueR.toString) < 0
+      case (AttributeValue.Map(valueL), AttributeValue.Map(valueR))             =>
+        valueL.toString.compareTo(valueR.toString) < 0
+      case (AttributeValue.Number(valueL), AttributeValue.Number(valueR))       =>
+        valueL.compareTo(valueR) < 0
+      case (AttributeValue.Null, AttributeValue.Null)                           =>
+        false
+      case (AttributeValue.String(valueL), AttributeValue.String(valueR))       =>
+        valueL.compareTo(valueR) < 0
+      case (AttributeValue.StringSet(valueL), AttributeValue.StringSet(valueR)) =>
+        valueL.toString.compareTo(valueR.toString) < 0
+      case _                                                                    => false
+    }
 }
 
 object FakeDynamoDBExecutor {
@@ -97,6 +163,24 @@ object FakeDynamoDBExecutor {
         case DeleteItem(tableName, key, conditionExpression, capacity, itemMetrics, returnValues) =>
           println(s"$tableName $key $conditionExpression $capacity $itemMetrics $returnValues")
           dbRef.update(db => db.remove(tableName.value, key).getOrElse(db)).unit
+
+        case QuerySome(
+              tableName,
+              indexName,
+              _,
+              exclusiveStartKey,
+              _,
+              _,
+              limit,
+              _,
+              _,
+              _,
+              _
+            ) =>
+          println(
+            s"$tableName, $indexName, $exclusiveStartKey, $limit"
+          )
+          ZIO.succeed((Chunk(emptyItem), None))
 
         // TODO: implement remaining constructors
 
--- dynamodb/src/test/scala/zio/dynamodb/FakeDynamoDBSpec.scala
+++ dynamodb/src/test/scala/zio/dynamodb/FakeDynamoDBSpec.scala
@@ -10,9 +10,15 @@ object FakeDynamoDBSpec extends DefaultRunnableSpec with BatchingFixtures {
   override def spec: ZSpec[Environment, Failure] =
     suite("FakeDynamoDB")(fakeDynamoDbSuite, fakeDatabaseSuite)
 
-  private val dbWithTwoTables   = Database()
+  private val dbWithTwoTables                                = Database()
     .table(tableName1.value, "k1")(primaryKey1 -> item1, primaryKey1_2 -> item1_2)
     .table(tableName3.value, "k3")(primaryKey3 -> item3)
+
+  private def databaseEntries(r: Range, pkFieldName: String) =
+    r.map(i => (PrimaryKey(pkFieldName -> i), Item(pkFieldName -> i, "k2" -> (i + 1)))).toList
+
+  private val dbWithTenItems                                 = Database()
+    .table(tableName1.value, "k1")(databaseEntries(0 to 9, "k1"): _*)
 
   private val fakeDatabaseSuite = suite("FakeDatabase suite")(
     test("getItem returns Some item when created using table()") {
@@ -48,7 +54,32 @@ object FakeDynamoDBSpec extends DefaultRunnableSpec with BatchingFixtures {
       )(
         equalTo(None)
       )
+    },
+    test("""scanSome("T1", None, 3) on 10 Items""") {
+      val db           = dbWithTenItems
+      val (chunk, lek) = db.scanSome("T1", None, 3)
+//      println(s"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX chunk=$chunk")
+      assert(chunk.length)(equalTo(3)) && assert(lek)(isSome)
+    },
+    test("""scanSome("T1", Some(PrimaryKey("k1" -> 4)), 3) on 10 Items""") {
+      val db           = dbWithTenItems
+      val (chunk, lek) = db.scanSome("T1", Some(PrimaryKey("k1" -> 4)), 3)
+//      println(s"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX chunk=$chunk lek=$lek")
+      assert(chunk.length)(equalTo(3)) && assert(lek)(isSome)
+    },
+    test("""scanSome("T1", Some(PrimaryKey("k1" -> 7)), 3) on 10 Items""") {
+      val db           = dbWithTenItems
+      val (chunk, lek) = db.scanSome("T1", Some(PrimaryKey("k1" -> 7)), 3)
+//      println(s"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX chunk=$chunk lek=$lek")
+      assert(chunk.length)(equalTo(2)) && assert(lek)(isSome)
     }
+    // TODO
+//    test("""scanSome("T1", Some(PrimaryKey("k1" -> 9)), 3) on 10 Items""") {
+//      val db           = dbWithTenItems
+//      val (chunk, lek) = db.scanSome("T1", Some(PrimaryKey("k1" -> 9)), 3)
+//      println(s"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX chunk=$chunk lek=$lek")
+//      assert(chunk.length)(equalTo(0)) && assert(lek)(isNone)
+//    }
   )
 
   private val fakeDynamoDbSuite = suite("FakeDynamoDB suite")(
