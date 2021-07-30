a34db7c0b9d69e6719def50cd217fc8725b8bc3d
Avinder Bahra <avinder.bahra@gmail.com>
2021-07-30 07:34:07 +0100

    added BatchFromStreamExamples. Batch functions now return a ZIO rather than a stream

--- dynamodb/src/main/scala/zio/dynamodb/BatchFromStream.scala
+++ dynamodb/src/main/scala/zio/dynamodb/BatchFromStream.scala
@@ -7,33 +7,39 @@ import zio.stream.ZStream
 object BatchFromStream {
 
   def batchWriteFromStream[R, A](
-    mPar: Int,
+    mPar: Int, // TODO make last param
     stream: ZStream[R, Exception, A]
-  )(f: A => DynamoDBQuery.Write[Unit]): ZStream[DynamoDBExecutor with R, Exception, Unit] =
-    stream.grouped(25).mapMPar(mPar) { chunk =>
-      val zio = DynamoDBQuery
-        .forEach(chunk)(a => f(a))
-      for {
-        r <- ZIO.environment[DynamoDBExecutor]
-        _ <- zio.execute.provide(r)
-      } yield ()
-    }
+  )(f: A => DynamoDBQuery.Write[Unit]): ZIO[DynamoDBExecutor with R, Exception, Unit] =
+    stream
+      .grouped(25)
+      .mapMPar(mPar) { chunk =>
+        val zio = DynamoDBQuery
+          .forEach(chunk)(a => f(a))
+        for {
+          r <- ZIO.environment[DynamoDBExecutor]
+          _ <- zio.execute.provide(r)
+        } yield ()
+      }
+      .runDrain
 
   def batchReadFromStream[R, A](
     tableName: String,
-    mPar: Int = 4,
+    mPar: Int = 4, // TODO make last param
     stream: ZStream[R, Exception, A]
   )(
     pk: A => PrimaryKey
-  )(f: List[Option[Item]] => ZIO[R, Exception, Unit]): ZStream[R with DynamoDBExecutor, Exception, Unit] =
-    stream.grouped(100).mapMPar(mPar) { chunk =>
-      val zio: DynamoDBQuery[List[Option[Item]]] = DynamoDBQuery
-        .forEach(chunk)(a => DynamoDBQuery.getItem(tableName, pk(a)))
-      for {
-        r    <- ZIO.environment[DynamoDBExecutor]
-        list <- zio.execute.provide(r)
-        _    <- f(list)
-      } yield ()
-    }
+  )(f: List[Option[Item]] => ZIO[R, Exception, Unit]): ZIO[R with DynamoDBExecutor, Exception, Unit] =
+    stream
+      .grouped(100)
+      .mapMPar(mPar) { chunk =>
+        val zio: DynamoDBQuery[List[Option[Item]]] = DynamoDBQuery
+          .forEach(chunk)(a => DynamoDBQuery.getItem(tableName, pk(a)))
+        for {
+          r    <- ZIO.environment[DynamoDBExecutor]
+          list <- zio.execute.provide(r)
+          _    <- f(list)
+        } yield ()
+      }
+      .runDrain
 
 }
---
+++ examples/src/main/scala/zio/dynamodb/examples/BatchFromStreamExamples.scala
@@ -0,0 +1,48 @@
+package zio.dynamodb.examples
+
+import zio.console.{ putStrLn, Console }
+import zio.dynamodb.DynamoDBExecutor.DynamoDBExecutor
+import zio.dynamodb.DynamoDBQuery.{ putItem, scanAll }
+import zio.dynamodb.fake.{ Database, FakeDynamoDBExecutor }
+import zio.dynamodb.{ BatchFromStream, Item, PrimaryKey }
+import zio.stream.ZStream
+import zio.{ App, ExitCode, URIO, ZIO }
+
+object BatchFromStreamExamples extends App {
+  private val executorLayer = FakeDynamoDBExecutor(Database().table("table1", "id")())
+
+  final case class Person(id: Int, name: String)
+
+  private val personStream: ZStream[Any, Nothing, Person] =
+    ZStream.fromIterable(1 to 100).map(i => Person(i, s"name$i"))
+
+  // read stream and do batched writes (grouped by 25 items) to Fake DDB
+  private val batchWriteProgram: ZIO[Console with DynamoDBExecutor, Exception, Unit] =
+    for {
+      _      <- BatchFromStream
+                  .batchWriteFromStream(4, personStream) { person =>
+                    putItem("table1", Item("id" -> person.id, "name" -> person.name))
+                  }
+      stream <- scanAll("table1", "id").execute
+      _      <- stream.tap(person => putStrLn(s"person=$person")).runDrain
+    } yield ()
+
+  // read stream to create batched GetItems (grouped by 100 items) which are executed against Fake DDB
+  private val batchReadProgram: ZIO[Console with DynamoDBExecutor, Exception, Unit] =
+    for {
+      // TODO: why does batchReadFromStream not infer automatically?
+      _ <- BatchFromStream.batchReadFromStream[Console, Person]("table1", stream = personStream)(person =>
+             PrimaryKey("id" -> person.id)
+           ) { xs =>
+             val value: ZIO[Console, Exception, Unit] =
+               putStrLn(s"list=${xs.mkString("\n")}")
+             value
+           }
+    } yield ()
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    (for {
+      _ <- batchWriteProgram
+      _ <- batchReadProgram
+    } yield ()).provideCustomLayer(executorLayer).exitCode
+}
