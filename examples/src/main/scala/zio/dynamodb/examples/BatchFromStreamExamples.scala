b477f24ef53ad888f2ada318f6c7933266a3e29e
Avinder Bahra <avinder.bahra@gmail.com>
2021-07-31 08:15:00 +0100

    inlined vals.

--- examples/src/main/scala/zio/dynamodb/examples/BatchFromStreamExamples.scala
+++ examples/src/main/scala/zio/dynamodb/examples/BatchFromStreamExamples.scala
@@ -1,12 +1,11 @@
 package zio.dynamodb.examples
 
 import zio.console.{ putStrLn, Console }
-import zio.dynamodb.DynamoDBExecutor.DynamoDBExecutor
-import zio.dynamodb.DynamoDBQuery.{ putItem, scanAll }
+import zio.dynamodb.DynamoDBQuery.putItem
 import zio.dynamodb.fake.{ Database, FakeDynamoDBExecutor }
 import zio.dynamodb.{ BatchFromStream, Item, PrimaryKey }
 import zio.stream.ZStream
-import zio.{ App, ExitCode, URIO, ZIO }
+import zio.{ App, ExitCode, URIO }
 
 object BatchFromStreamExamples extends App {
   private val executorLayer = FakeDynamoDBExecutor.layer(Database().table("table1", "id")())
@@ -16,31 +15,16 @@ object BatchFromStreamExamples extends App {
   private val personStream: ZStream[Any, Nothing, Person] =
     ZStream.fromIterable(1 to 100).map(i => Person(i, s"name$i"))
 
-  // read stream and do batched writes (grouped by 25 items) to Fake DDB
-  private val batchWriteProgram: ZIO[Console with DynamoDBExecutor, Exception, Unit] =
-    for {
-      _      <- BatchFromStream
-                  .batchWriteFromStream(personStream) { person =>
-                    putItem("table1", Item("id" -> person.id, "name" -> person.name))
-                  }
-                  .runDrain
-      stream <- scanAll("table1", "id").execute
-      _      <- stream.tap(person => putStrLn(s"person=$person")).runDrain
-    } yield ()
-
-  // read stream to create batched GetItems (grouped by 100 items) which are executed against Fake DDB
-  private val batchReadProgram: ZIO[Console with DynamoDBExecutor, Exception, Unit] =
-    for {
-      // TODO: why does batchReadFromStream not infer automatically?
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    (for {
+      _ <- BatchFromStream
+             .batchWriteFromStream(personStream) { person =>
+               putItem("table1", Item("id" -> person.id, "name" -> person.name))
+             }
+             .runDrain
       _ <- BatchFromStream
              .batchReadFromStream[Console, Person]("table1", personStream)(person => PrimaryKey("id" -> person.id))
              .mapMPar(4)(item => putStrLn(s"item=$item"))
              .runDrain
-    } yield ()
-
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    (for {
-      _ <- batchWriteProgram
-      _ <- batchReadProgram
     } yield ()).provideCustomLayer(executorLayer).exitCode
 }
