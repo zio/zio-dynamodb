f5c0d5143645d2787445c98bc9eb2ca779add8aa
Avinder Bahra <avinder.bahra@gmail.com>
2021-09-03 08:26:18 +0100

    very basic encoder working

--- examples/src/main/scala/zio/dynamodb/examples/CodecGeneratorExperiment.scala
+++ examples/src/main/scala/zio/dynamodb/examples/CodecGeneratorExperiment.scala
@@ -5,7 +5,8 @@ import zio.dynamodb.{ AttrMap, AttributeValue }
 import zio.schema.{ Schema, StandardType }
 
 object CodecGeneratorExperiment extends App {
-  type Encoder[A] = A => AttributeValue
+  type AVEncoder[A]      = A => AttributeValue
+  type AttrMapEncoder[A] = A => AttrMap
 
   final case class SimpleCaseClass(id: Int, name: String)
 
@@ -13,44 +14,49 @@ object CodecGeneratorExperiment extends App {
     Chunk.empty,
     Schema.Field("id", Schema[Int]),
     Schema.Field("name", Schema[String]),
-    SimpleCaseClass(_, _),
+    SimpleCaseClass,
     _.id,
     _.name
   )
 
-  def encoder[A](schema: Schema[A]): Option[Encoder[A]] =
+  def attrMapEncoder[A](schema: Schema[A]): Option[AttrMapEncoder[A]] =
     schema match {
       case Schema.CaseClass2(_, field1, field2, _, extractField1, extractField2) =>
         caseClassEncoder(field1 -> extractField1, field2 -> extractField2)
       case _                                                                     => None
     }
 
-  def caseClassEncoder[B](fields: (Schema.Field[_], B => Any)*): Option[Encoder[B]] =
+  def caseClassEncoder[B](fields: (Schema.Field[_], B => Any)*): Option[AttrMapEncoder[B]] =
     Some { (b: B) =>
-      println(s"$fields $b")
-      fields.foreach {
-        case (Schema.Field(key, schema, _), ext) =>
-          val enc: Option[Encoder[Any]] = schemaEncoder(schema)
-          val extractedFieldValue       = ext(b)
-          val maybeAttributeValue       = enc.map(_(extractedFieldValue))
-          println(s"$key $schema $ext $maybeAttributeValue, ")
+      val attrMap: AttrMap = fields.foldRight[AttrMap](AttrMap.empty) {
+        case ((Schema.Field(key, schema, _), ext), acc) =>
+          val enc: Option[AVEncoder[Any]] = schemaEncoder(schema)
+          val extractedFieldValue         = ext(b)
+          val maybeAttributeValue         = enc.map(_(extractedFieldValue))
+          println(s"$key $schema $ext $maybeAttributeValue")
+
+          // TODO: for now ignore errors
+          val attrMap = maybeAttributeValue.fold(AttrMap.empty)(av => AttrMap(key -> av))
+
+          acc ++ attrMap
       }
-      AttributeValue.Null
+
+      attrMap
     }
 
-  def schemaEncoder[A](schema: Schema[A]): Option[Encoder[A]] =
+  def schemaEncoder[A](schema: Schema[A]): Option[AVEncoder[A]] =
     schema match {
       case Schema.Primitive(standardType) => primitiveEncoder(standardType)
       case _                              => None
     }
 
-  def primitiveEncoder[A](standardType: StandardType[A]): Option[Encoder[A]] =
+  def primitiveEncoder[A](standardType: StandardType[A]): Option[AVEncoder[A]] =
     standardType match {
       case StandardType.StringType => Some((a: A) => AttributeValue.String(a.toString))
       case StandardType.IntType    => Some((a: A) => AttributeValue.Number(BigDecimal(a.toString)))
       case _                       => None
     }
 
-  val x: Option[AttributeValue] = encoder(simpleCaseClassSchema).map(_(SimpleCaseClass(42, "Avi")))
+  val x: Option[AttrMap] = attrMapEncoder(simpleCaseClassSchema).map(_(SimpleCaseClass(42, "Avi")))
   println(x)
 }
