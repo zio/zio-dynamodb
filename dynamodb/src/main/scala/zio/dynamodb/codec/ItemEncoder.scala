b04438233d1a2fc3e216a162cdfe906253d1fa57
Avinder Bahra <avinder.bahra@gmail.com>
2021-09-11 11:28:51 +0100

    removed unnecessary optionality

--- dynamodb/src/main/scala/zio/dynamodb/codec/ItemEncoder.scala
+++ dynamodb/src/main/scala/zio/dynamodb/codec/ItemEncoder.scala
@@ -15,50 +15,43 @@ object AttrMapEncoder {
 }
 
 object ItemEncoder {
-  def toItem[A](a: A)(implicit schema: Schema[A]): Item = unsafeSchemaEncoder(schema, a)
+  def toItem[A](a: A)(implicit schema: Schema[A]): Item = unsafeSchemaEncoder(schema, "top_level_key_ignored")(a)
 
-  // temp function until all attribute mappings are done
-  def unsafeSchemaEncoder[A](schema: Schema[A], a: A): Item =
-    schemaEncoder(schema, "top_level_key_ignored").getOrElse(throw new Exception(s"problems encoding $a"))(a)
-
-  // TODO: remove Option on return type when all encodings are implemented
+  // TODO: remove unsafe prefix when all encodings are implemented
   @tailrec
-  def schemaEncoder[A](schema: Schema[A], key: String): Option[AttrMapEncoder[A]] =
+  def unsafeSchemaEncoder[A](schema: Schema[A], key: String): AttrMapEncoder[A] =
     schema match {
       case ProductEncoder(encoder)        =>
-        Some(encoder)
+        encoder
       case Schema.Primitive(standardType) =>
-        primitiveEncoder(standardType).map(AttrMapEncoder.fromAvEncoder(key, _))
+        AttrMapEncoder.fromAvEncoder(key, primitiveEncoder(standardType))
 
       // TODO: why do we need this?
-      case l @ Schema.Lazy(_)             => schemaEncoder(l.schema, key)
+      case l @ Schema.Lazy(_)             => unsafeSchemaEncoder(l.schema, key)
 
       case _ =>
-        None
+        throw new UnsupportedOperationException(s"schema $schema not yet supported")
     }
 
   object ProductEncoder {
     def unapply[A](schema: Schema[A]): Option[AttrMapEncoder[A]] =
       schema match {
         case Schema.CaseClass2(_, field1, field2, _, extractField1, extractField2)                        =>
-          caseClassEncoder(field1 -> extractField1, field2 -> extractField2)
+          Some(caseClassEncoder(field1 -> extractField1, field2 -> extractField2))
         case Schema.CaseClass3(_, field1, field2, field3, _, extractField1, extractField2, extractField3) =>
-          caseClassEncoder(field1 -> extractField1, field2 -> extractField2, field3 -> extractField3)
+          Some(caseClassEncoder(field1 -> extractField1, field2 -> extractField2, field3 -> extractField3))
         case _                                                                                            =>
           None
       }
   }
 
-  def caseClassEncoder[Z](fields: (Schema.Field[_], Z => Any)*): Option[AttrMapEncoder[Z]] =
-    Some(AttrMapEncoder { (z: Z) =>
+  def caseClassEncoder[Z](fields: (Schema.Field[_], Z => Any)*): AttrMapEncoder[Z] =
+    AttrMapEncoder { (z: Z) =>
       val attrMap: AttrMap = fields.foldRight[AttrMap](AttrMap.empty) {
         case ((Schema.Field(key, schema, _), ext), acc) =>
-          val enc: Option[AttrMapEncoder[Any]] = schemaEncoder(schema, key)
-          val extractedFieldValue              = ext(z)
-          val maybeAttrMap: Option[AttrMap]    = enc.map(_(extractedFieldValue))
-
-          // TODO: for now ignore errors
-          val attrMap = maybeAttrMap.getOrElse(AttrMap.empty)
+          val enc: AttrMapEncoder[Any] = unsafeSchemaEncoder(schema, key)
+          val extractedFieldValue      = ext(z)
+          val attrMap: AttrMap         = enc(extractedFieldValue)
 
           @tailrec
           def combineAttrMaps[A](schema: Schema[A]): AttrMap =
@@ -75,17 +68,18 @@ object ItemEncoder {
       }
 
       attrMap
-    })
+    }
 
-  def primitiveEncoder[A](standardType: StandardType[A]): Option[ToAttributeValue[A]] =
+  def primitiveEncoder[A](standardType: StandardType[A]): ToAttributeValue[A] =
     standardType match {
       case StandardType.BoolType   =>
-        Some((a: A) => AttributeValue.Bool(a.asInstanceOf[Boolean])) // TODO: try to use ToAttributeValue machinery
-      case StandardType.StringType => Some((a: A) => AttributeValue.String(a.toString))
+        (a: A) => AttributeValue.Bool(a.asInstanceOf[Boolean]) // TODO: try to use ToAttributeValue machinery
+      case StandardType.StringType => (a: A) => AttributeValue.String(a.toString)
       case StandardType.ShortType | StandardType.IntType | StandardType.LongType | StandardType.FloatType |
           StandardType.DoubleType =>
-        Some((a: A) => AttributeValue.Number(BigDecimal(a.toString)))
-      case _                       => None
+        (a: A) => AttributeValue.Number(BigDecimal(a.toString))
+      case _                       =>
+        throw new UnsupportedOperationException(s"StandardType $standardType not yet supported")
     }
 
 }
