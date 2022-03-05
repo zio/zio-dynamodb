ea6941682b52ac3d78ad7b5aa45e0a87c8b22321
Avinder Bahra <avinder.bahra@gmail.com>
2022-03-05 09:23:15 +0000

    restored commented out examples

--- examples/src/main/scala/zio/dynamodb/examples/ConditionExpressionExamples.scala
+++ examples/src/main/scala/zio/dynamodb/examples/ConditionExpressionExamples.scala
@@ -1,32 +1,31 @@
-//package zio.dynamodb.examples
-//
-//import zio.dynamodb.ProjectionExpression._
-//import zio.dynamodb._
-//
-//object ConditionExpressionExamples {
-//
-//  val sizeOnRhs: ConditionExpression = AttributeValue(1) > $("col2").size
-//  val avToCond1: ConditionExpression = AttributeValue("2") > $("col1")
-//  val avToCond2: ConditionExpression = AttributeValue("2") < $("col1")
-//
-//  val exists: ConditionExpression     = $("col1").exists
-//  val notExists: ConditionExpression  = $("col1").notExists
-//  val beginsWith: ConditionExpression =
-//    $("col1").beginsWith("1") // TODO: "the type stabilizer$1.To must be a string in order to use this operator"
-//  val contains: ConditionExpression  = $("col1").contains("1")
-//  val sizeOnLhs: ConditionExpression = $("col2").size > 1
-//  val isType: ConditionExpression    = $("col1").isNumber
-//  val expnAnd: ConditionExpression   = beginsWith && sizeOnLhs
-//  val expnOr: ConditionExpression    = beginsWith || sizeOnLhs
-//  val expnNot: ConditionExpression   = !beginsWith
-//
-//  val peCompPe1: ConditionExpression = $("col1") > $("col2")
-//  val peCompPe2: ConditionExpression = $("col1") === $("col2")
-//
-//  val peToCond1: ConditionExpression   = $("col1") === "2"
-//  val peToCond2: ConditionExpression   = $("col1") > 1.0
-//  val peFnToCond1: ConditionExpression = $("col1").between("1", "2")
-//  val peFnToCond2: ConditionExpression = $("col1").in(Set("1", "2"))
-//  val peFnToCond3: ConditionExpression = $("col1").in("1", "2")
-//
-//}
+package zio.dynamodb.examples
+
+import zio.dynamodb.ProjectionExpression._
+import zio.dynamodb._
+
+object ConditionExpressionExamples {
+
+  val sizeOnRhs: ConditionExpression = AttributeValue(1) > $("col2").size
+  val avToCond1: ConditionExpression = AttributeValue("2") > $("col1")
+  val avToCond2: ConditionExpression = AttributeValue("2") < $("col1")
+
+  val exists: ConditionExpression     = $("col1").exists
+  val notExists: ConditionExpression  = $("col1").notExists
+  val beginsWith: ConditionExpression = $("col1").beginsWith("1")
+  val contains: ConditionExpression   = $("col1").contains("1")
+  val sizeOnLhs: ConditionExpression  = $("col2").size > 1
+  val isType: ConditionExpression     = $("col1").isNumber
+  val expnAnd: ConditionExpression    = beginsWith && sizeOnLhs
+  val expnOr: ConditionExpression     = beginsWith || sizeOnLhs
+  val expnNot: ConditionExpression    = !beginsWith
+
+  val peCompPe1: ConditionExpression = $("col1") > $("col2")
+  val peCompPe2: ConditionExpression = $("col1") === $("col2")
+
+  val peToCond1: ConditionExpression   = $("col1") === "2"
+  val peToCond2: ConditionExpression   = $("col1") > 1.0
+  val peFnToCond1: ConditionExpression = $("col1").between("1", "2")
+  val peFnToCond2: ConditionExpression = $("col1").in(Set("1", "2"))
+  val peFnToCond3: ConditionExpression = $("col1").in("1", "2")
+
+}
