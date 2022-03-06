2601b8880b14f2a73ea841e6d61cfea25f9c1a61
Avinder Bahra <avinder.bahra@gmail.com>
2022-03-06 12:54:33 +0000

    added comment

--- examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/StudentZioDynamoDbExampleWithOptics.scala
+++ examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/StudentZioDynamoDbExampleWithOptics.scala
@@ -104,7 +104,7 @@ object StudentZioDynamoDbExampleWithOptics extends App {
                     .execute
     _          <- updateItem("student", PrimaryKey("email" -> "avi@gmail.com", "subject" -> "maths")) {
                     enrollmentDate.setValue(enrolDate2.toString) + payment.setValue(Payment.PayPal.toString) + address
-                      .set[Option[Address]](
+                      .set[Option[Address]]( // Note we are setting a case class directly here
                         Some(Address("line1", "postcode1"))
                       )
                   }.execute
