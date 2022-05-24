6fcddd2f526c2f97fc6b75fff97d3206c7619a06
nhyne <nhyne@nhyne.dev>
2022-05-23 22:17:16 -0400

    not batching condition expression delete/put items
    also properly rendering the condition expressions in put/delete items.

--- dynamodb/src/it/scala/zio/dynamodb/LiveSpec.scala
+++ dynamodb/src/it/scala/zio/dynamodb/LiveSpec.scala
@@ -214,16 +214,23 @@ object LiveSpec extends DefaultRunnableSpec {
             getItem(tableName, PrimaryKey(id -> "nowhere", number -> 1000)).execute.map(item => assert(item)(isNone))
           }
         },
-        testM("delete item with where clause") {
+        testM("delete item with false where clause") {
           withDefaultTable { tableName =>
             val deleteItem = DeleteItem(
               key = pk(avi3Item),
               tableName = TableName(tableName)
             ).where($("firstName").beginsWith("noOne"))
-            for {
-              _                <- deleteItem.execute
-              maybeDeletedItem <- getItem(tableName, pk(avi3Item)).execute
-            } yield assert(maybeDeletedItem)(isSome)
+            assertM(deleteItem.execute.run)(fails(assertDynamoDbException("The conditional request failed")))
+          }
+        },
+        testM("put item with false where clause") {
+          withDefaultTable { tableName =>
+            val putItem = PutItem(
+              tableName = TableName(tableName),
+              item = Item(id -> "nothing", number -> 900)
+            ).where($("id").beginsWith("false"))
+
+            assertM(putItem.execute.run)(fails(assertDynamoDbException("The conditional request failed")))
           }
         },
         testM("batch get item") {
@@ -747,7 +754,13 @@ object LiveSpec extends DefaultRunnableSpec {
 
               assertM(
                 conditionCheck.zip(putItem).transaction.execute.run
-              )(fails(assertDynamoDbException("ConditionalCheckFailed")))
+              )(
+                fails(
+                  assertDynamoDbException(
+                    "Transaction cancelled, please refer cancellation reasons for specific reasons [ConditionalCheckFailed, None]"
+                  )
+                )
+              )
             }
           },
           testM("delete item") {
