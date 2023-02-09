0bdf77d54eeec30f445a522a985aaa23a76a3ff1
avinder.bahra <avinder.bahra@gmail.com>
2023-02-09 09:03:29 +0000

    created reproducer

---
+++ examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/PEExperiment.scala
@@ -0,0 +1,39 @@
+package zio.dynamodb.examples.dynamodblocal
+
+import zio.dynamodb.DynamoDBQuery._
+import zio.dynamodb._
+import zio.dynamodb.examples.dynamodblocal.DynamoDB._
+import zio.schema.DeriveSchema
+import zio.{ Console, ZIOAppDefault }
+import zio.schema.annotation.fieldName
+
+object PEExperiment extends ZIOAppDefault {
+
+  /*
+  Possible fixes:
+  1) @fieldName("myTtl") ttl: Option[Long] - works
+  2) @fieldName("ttl") myTtl: Option[Long] - does not work
+   */
+
+  final case class RecordWithTtl(
+    email: String,
+    @fieldName("alt_ttl") ttl: Option[Long]
+  )
+
+  object RecordWithTtl {
+    implicit val schema = DeriveSchema.gen[RecordWithTtl]
+    val (email, myTtl)  = ProjectionExpression.accessors[RecordWithTtl]
+  }
+
+  private val program = for {
+    _   <- createTable("withttl", KeySchema("email"), BillingMode.PayPerRequest)(
+             AttributeDefinition.attrDefnString("email")
+           ).execute
+    _   <- put("withttl", RecordWithTtl("email1", Some(10L))).execute
+    rec <- get[RecordWithTtl]("withttl", PrimaryKey("email" -> "email1")).execute.absolve
+    s   <- scanAll[RecordWithTtl]("withttl").filter(RecordWithTtl.myTtl === Some(10L)).execute.flatMap(_.runCollect)
+    _   <- Console.printLine(s"XXXXXXXXXXXXX $s")
+  } yield rec
+
+  override def run = program.provide(layer)
+}
