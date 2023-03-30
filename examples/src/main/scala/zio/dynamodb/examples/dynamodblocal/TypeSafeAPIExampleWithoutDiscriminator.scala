ec2b74dd29257bfebd81a5cd40441966e3010705
Avinder Bahra <avinder.bahra@gmail.com>
2023-03-30 08:01:07 +0100

    add implicit type hint

--- examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/TypeSafeAPIExampleWithoutDiscriminator.scala
+++ examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/TypeSafeAPIExampleWithoutDiscriminator.scala
@@ -6,6 +6,7 @@ import zio.dynamodb._
 import zio.dynamodb.examples.dynamodblocal.TypeSafeAPIExampleWithoutDiscriminator.TrafficLight.{ Amber, Box, Green }
 import zio.schema.DeriveSchema
 import zio.{ Console, ZIOAppDefault }
+import zio.schema.Schema
 
 object TypeSafeAPIExampleWithoutDiscriminator extends ZIOAppDefault {
 
@@ -13,26 +14,28 @@ object TypeSafeAPIExampleWithoutDiscriminator extends ZIOAppDefault {
   object TrafficLight {
     final case class Green(rgb: Int) extends TrafficLight
     object Green {
-      implicit val schema = DeriveSchema.gen[Green]
-      val rgb             = ProjectionExpression.accessors[Green]
+      implicit val schema: Schema.CaseClass1.WithFields["rgb", Int, Green] = DeriveSchema.gen[Green]
+      val rgb                                                              = ProjectionExpression.accessors[Green]
     }
     final case class Red(rgb: Int) extends TrafficLight
     object Red   {
-      implicit val schema = DeriveSchema.gen[Red]
-      val rgb             = ProjectionExpression.accessors[Red]
+      implicit val schema: Schema.CaseClass1.WithFields["rgb", Int, Red] = DeriveSchema.gen[Red]
+      val rgb                                                            = ProjectionExpression.accessors[Red]
     }
     final case class Amber(rgb: Int) extends TrafficLight
     object Amber {
-      implicit val schema = DeriveSchema.gen[Amber]
-      val rgb             = ProjectionExpression.accessors[Amber]
+      implicit val schema: Schema.CaseClass1.WithFields["rgb", Int, Amber] = DeriveSchema.gen[Amber]
+      val rgb                                                              = ProjectionExpression.accessors[Amber]
     }
     final case class Box(id: Int, code: Int, trafficLightColour: TrafficLight)
     object Box   {
-      implicit val schema                = DeriveSchema.gen[Box]
-      val (id, code, trafficLightColour) = ProjectionExpression.accessors[Box]
+      implicit val schema
+        : Schema.CaseClass3.WithFields["id", "code", "trafficLightColour", Int, Int, TrafficLight, Box] =
+        DeriveSchema.gen[Box]
+      val (id, code, trafficLightColour)                                                                = ProjectionExpression.accessors[Box]
     }
 
-    implicit val schema = DeriveSchema.gen[TrafficLight]
+    implicit val schema: Schema.Enum3[Green, Red, Amber, TrafficLight] = DeriveSchema.gen[TrafficLight]
 
     val (green, red, amber) = ProjectionExpression.accessors[TrafficLight]
   }
