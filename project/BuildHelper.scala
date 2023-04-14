0dfabf4ae99196b2dad84d212a39107d8d953ec2
Scala Steward <scala_steward@virtuslab.com>
2023-04-14 00:22:41 +0000

    Update scala3-library to 3.2.2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ object BuildHelper {
   // Align with zio-schema since we have a deep dependency on it
   val Scala212                = "2.12.17"
   val Scala213                = "2.13.8"
-  val Scala3                  = "3.2.1"
+  val Scala3                  = "3.2.2"
   private val SilencerVersion = "1.7.12"
 
   private val stdOptions = Seq(
