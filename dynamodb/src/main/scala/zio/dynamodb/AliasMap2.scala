50b79e3c6ada84c33cdb3f18bb87033a18f802cb
Avinder Bahra <avinder.bahra@gmail.com>
2023-04-22 10:18:19 +0100

    disambiguate path segment when used as a key

--- dynamodb/src/main/scala/zio/dynamodb/AliasMap2.scala
+++ dynamodb/src/main/scala/zio/dynamodb/AliasMap2.scala
@@ -16,13 +16,13 @@ private[dynamodb] final case class AliasMap2 private[dynamodb] (map: Map[AliasMa
           acc // identity
         case ProjectionExpression.MapElement(ProjectionExpression.Root, name) =>
           val nameAlias = s"#n${acc._1.index}"
-          val mapTmp    = acc._1.map + ((AliasMap2.PathSegment(name), nameAlias))
+          val mapTmp    = acc._1.map + ((AliasMap2.PathSegment(ProjectionExpression.Root, name), nameAlias))
           val xs        = acc._2 :+ nameAlias
           val map       = mapTmp + ((AliasMap2.FullPath(pe), xs.reverse.mkString(""))) // cache final result
           (AliasMap2(map, self.index + 1), xs)
         case ProjectionExpression.MapElement(parent, key)                     =>
           val nameAlias = s"#n${acc._1.index}"
-          val next      = AliasMap2(acc._1.map + ((AliasMap2.PathSegment(key), nameAlias)), acc._1.index + 1)
+          val next      = AliasMap2(acc._1.map + ((AliasMap2.PathSegment(parent, key), nameAlias)), acc._1.index + 1)
           loop(parent, (next, acc._2 :+ ("." + key)))
         case ProjectionExpression.ListElement(parent, index)                  =>
           loop(parent, (acc._1, acc._2 :+ s"[$index]"))
@@ -53,7 +53,8 @@ private[dynamodb] object AliasMap2 {
 
   sealed trait Key
   final case class AttributeValueKey(av: AttributeValue)                    extends Key
-  final case class PathSegment(segment: String)                             extends Key
+  // we include parent to disambiguate PathSegment as a Map key for cases where the same segment name is used multiple times
+  final case class PathSegment[From, To](parent: ProjectionExpression[From, To], segment: String)  extends Key
   // used to cache the final substituted path - is this necessary????
   final case class FullPath[From, To](path: ProjectionExpression[From, To]) extends Key
 
--- dynamodb/src/main/scala/zio/dynamodb/DynamoDBExecutorImpl.scala
+++ dynamodb/src/main/scala/zio/dynamodb/DynamoDBExecutorImpl.scala
@@ -604,7 +604,7 @@ case object DynamoDBExecutorImpl {
       val map = aliasMap.map.flatMap {
         case (AliasMap2.AttributeValueKey(attrVal), str) =>
           awsAttributeValue(attrVal).map(a => (ZIOAwsExpressionAttributeValueVariable(str), a))
-        case (AliasMap2.PathSegment(_), _)               =>
+        case (AliasMap2.PathSegment(_, _), _)               =>
           None
         case (AliasMap2.FullPath(_), _)                  =>
           None
@@ -621,7 +621,7 @@ case object DynamoDBExecutorImpl {
           None
         case (AliasMap2.FullPath(_), _)            =>
           None
-        case (AliasMap2.PathSegment(segment), str) =>
+        case (AliasMap2.PathSegment(_, segment), str) =>
           Some((ExpressionAttributeNameVariable(str), ZIOAwsAttributeName(segment)))
       }
       if (map.isEmpty) None else Some(map)
