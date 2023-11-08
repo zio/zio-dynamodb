3bac276f4156be157b143f9f10dc24a3e848ef6c
Avinder Bahra <avinder.bahra@gmail.com>
2023-11-08 07:01:38 +0000

    improve example

--- examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/CeInteropExample.scala
+++ examples/src/main/scala/zio/dynamodb/examples/dynamodblocal/CeInteropExample.scala
@@ -8,16 +8,18 @@ import zio.dynamodb.DynamoDBQuery.{ createTable, deleteTable, get, put }
 import cats.effect.std.Console
 import cats.effect.IO
 import cats.effect.IOApp
+import cats.syntax.all._
 
 import java.net.URI
 
-import zio.dynamodb.interop.ce.syntax._
+import zio.dynamodb.interop.ce.syntax2._
 import zio.dynamodb.ProjectionExpression
 import zio.schema.DeriveSchema
 import zio.dynamodb.KeySchema
 import zio.dynamodb.BillingMode
 import zio.dynamodb.AttributeDefinition
 import zio.dynamodb.DynamoDBQuery
+import cats.effect.kernel.Async
 
 /**
  * example cats effect interop application
@@ -34,13 +36,14 @@ object CeInteropExample extends IOApp.Simple {
     implicit val schema = DeriveSchema.gen[Person]
     val (id, name)      = ProjectionExpression.accessors[Person]
   }
-  // TODO: def program[F[_]]
-  val run = {
+
+  def program[F[_]](implicit F: Async[F]) = {
     implicit val runtime = zio.Runtime.default // DynamoDBExceutorF.of requires an implicit Runtime
+    val console          = Console.make[F]
 
     for {
       _ <- DynamoDBExceutorF
-             .ofCustomised[IO] { builder => // note only AWS SDK model is exposed here, not zio.aws
+             .ofCustomised[F] { builder => // note only AWS SDK model is exposed here, not zio.aws
                builder
                  .endpointOverride(URI.create("http://localhost:8000"))
                  .region(Region.US_EAST_1)
@@ -53,17 +56,18 @@ object CeInteropExample extends IOApp.Simple {
                               ).executeToF
                  _         <- put(tableName = "Person", Person(id = "avi", name = "Avinder")).executeToF
                  result    <- get(tableName = "Person")(Person.id.partitionKey === "avi").executeToF
-                 _         <- Console[IO].println(s"found=$result")
+                 _         <- console.println(s"found=$result")
                  fs2Stream <- DynamoDBQuery
                                 .scanAll[Person](tableName = "Person")
                                 .parallel(50)                                                        // server side parallel scan
                                 .filter(Person.name.beginsWith("Avi") && Person.name.contains("de")) // reified optics
                                 .executeToF
-                 _         <- fs2Stream.evalTap(person => Console[IO].println(s"person=$person")).compile.drain
+                 _         <- fs2Stream.evalTap(person => console.println(s"person=$person")).compile.drain
                  _         <- deleteTable("Person").executeToF
                } yield ()
              }
     } yield ()
   }
 
+  val run = program[IO]
 }
