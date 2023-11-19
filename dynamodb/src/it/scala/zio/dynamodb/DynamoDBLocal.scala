307fcfd39bea9d64dfecac0245c6d11f1cb4cd50
Avinder Bahra <avinder.bahra@gmail.com>
2023-11-19 17:07:46 +0000

    mote it tests, some cleanup

---
+++ dynamodb/src/it/scala/zio/dynamodb/DynamoDBLocal.scala
@@ -0,0 +1,33 @@
+package zio.dynamodb
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import zio.ZLayer
+import zio.aws.core.config
+import zio.aws.dynamodb
+import zio.aws.dynamodb.DynamoDb
+import zio.aws.netty
+import zio.dynamodb.DynamoDBExecutor
+
+import java.net.URI
+
+object DynamoDBLocal {
+  val awsConfig = ZLayer.succeed(
+    config.CommonAwsConfig(
+      region = None,
+      credentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create("dummy", "dummy")),
+      endpointOverride = None,
+      commonClientConfig = None
+    )
+  )
+
+  val dynamoDbLayer: ZLayer[Any, Throwable, DynamoDb] =
+    (netty.NettyHttpClient.default ++ awsConfig) >>> config.AwsConfig.default >>> dynamodb.DynamoDb.customized {
+      builder =>
+        builder.endpointOverride(URI.create("http://localhost:8000")).region(Region.US_EAST_1)
+    }
+
+  val dynamoDBExecutorLayer = dynamoDbLayer >>> DynamoDBExecutor.live
+
+}
