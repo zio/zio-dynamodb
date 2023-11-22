bea1f6628571372872322402e58cb0c12a27c33b
Avinder Bahra <avinder.bahra@gmail.com>
2023-11-22 07:54:43 +0000

    reduce scope of PR

--- dynamodb/src/it/scala/zio/dynamodb/DynamoDBLocalSpec.scala
+++
@@ -1,8 +0,0 @@
-package zio.dynamodb
-
-import zio.test.ZIOSpec
-import zio.ZLayer
-
-abstract class DynamoDBLocalSpec extends ZIOSpec[DynamoDBExecutor] {
-  override def bootstrap: ZLayer[Any, Nothing, DynamoDBExecutor] = DynamoDBLocal.dynamoDBExecutorLayer.orDie
-}
