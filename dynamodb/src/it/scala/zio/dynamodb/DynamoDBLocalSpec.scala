1f04986cdede6bc54d9e88bdf4634d18c842fe42
Avinder Bahra <avinder.bahra@gmail.com>
2023-11-25 07:43:00 +0000

    remove singleton

--- dynamodb/src/it/scala/zio/dynamodb/DynamoDBLocalSpec.scala
+++ dynamodb/src/it/scala/zio/dynamodb/DynamoDBLocalSpec.scala
@@ -1,8 +1,36 @@
 package zio.dynamodb
 
 import zio.test.ZIOSpec
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
 import zio.ZLayer
+import zio.aws.core.config
+import zio.aws.dynamodb
+import zio.aws.dynamodb.DynamoDb
+import zio.aws.netty
+
+import java.net.URI
 
 abstract class DynamoDBLocalSpec extends ZIOSpec[DynamoDBExecutor] {
-  override def bootstrap: ZLayer[Any, Nothing, DynamoDBExecutor] = DynamoDBLocal.dynamoDBExecutorLayer.orDie
+
+  private val awsConfig = ZLayer.succeed(
+    config.CommonAwsConfig(
+      region = None,
+      credentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create("dummy", "dummy")),
+      endpointOverride = None,
+      commonClientConfig = None
+    )
+  )
+
+  private val dynamoDbLayer: ZLayer[Any, Throwable, DynamoDb] =
+    (netty.NettyHttpClient.default ++ awsConfig) >>> config.AwsConfig.default >>> dynamodb.DynamoDb.customized {
+      builder =>
+        builder.endpointOverride(URI.create("http://localhost:8000")).region(Region.US_EAST_1)
+    }
+
+  private val dynamoDBExecutorLayer = dynamoDbLayer >>> DynamoDBExecutor.live
+
+  override def bootstrap: ZLayer[Any, Nothing, DynamoDBExecutor] = dynamoDBExecutorLayer.orDie
 }
