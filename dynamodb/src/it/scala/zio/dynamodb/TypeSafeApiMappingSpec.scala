855772ee076416ea0726df13202fab893d0ffda2
Avinder Bahra <avinder.bahra@gmail.com>
2023-12-15 14:56:40 +0000

    mapping IT tests

--- dynamodb/src/it/scala/zio/dynamodb/TypeSafeApiMappingSpec.scala
+++ dynamodb/src/it/scala/zio/dynamodb/TypeSafeApiMappingSpec.scala
@@ -60,11 +60,11 @@ object TypeSafeApiMappingSpec extends DynamoDBLocalSpec {
 
   override def spec: Spec[Environment with TestEnvironment with Scope, Any] =
     suite("all")(
-      topLevelSumTypeDiscriminatorNameSuite,
-      topLevelSumTypeNoDiscriminatorSuite
+      topLevelSumTypeSuite,
+      nestedSumTypeSuite
     ) @@ TestAspect.nondeterministic
 
-  val topLevelSumTypeDiscriminatorNameSuite = suite("top level sum type with discriminatorName")(
+  val topLevelSumTypeDiscriminatorNameSuite = suite("with @discriminatorName annotation")(
     test("put and get concrete sub type") {
       withSingleIdKeyTable { invoiceTable =>
         for {
@@ -100,11 +100,11 @@ object TypeSafeApiMappingSpec extends DynamoDBLocalSpec {
     }
   )
 
-  val topLevelSumTypeNoDiscriminatorSuite = suite("top level sum type with noDiscriminator")(
+  val topLevelSumTypeNoDiscriminatorSuite = suite("with @noDiscriminator annotation")(
     test("put and get concrete sub type") {
       withSingleIdKeyTable { invoiceTable =>
         for {
-          _       <- put(invoiceTable, InvoiceWithDiscriminatorName.Unpaid("1")).execute
+          _       <- put(invoiceTable, InvoiceWithNoDiscriminator.Unpaid("1")).execute
           invoice <- get(invoiceTable)(InvoiceWithNoDiscriminator.Unpaid.id.partitionKey === "1").execute.absolve
         } yield assertTrue(invoice == InvoiceWithNoDiscriminator.Unpaid("1"))
       }
@@ -134,5 +134,170 @@ object TypeSafeApiMappingSpec extends DynamoDBLocalSpec {
         ))
       }
     }
+  )
+
+  val topLevelSumTypeSuite =
+    suite("Top level sum type suite")(topLevelSumTypeDiscriminatorNameSuite, topLevelSumTypeNoDiscriminatorSuite)
+
+  sealed trait AbodeType
+  object AbodeType {
+    case object House extends AbodeType
+    case object Flat  extends AbodeType
+
+    implicit val schema: Schema.Enum2[House.type, Flat.type, AbodeType] = DeriveSchema.gen[AbodeType]
+    val (house, flat)                                                   = ProjectionExpression.accessors[AbodeType]
+  }
+
+  sealed trait AddressDefaultMapping {
+    def abodeType: AbodeType
+  }
+  object AddressDefaultMapping       {
+    final case class UKAddress(postcode: String, abodeType: AbodeType) extends AddressDefaultMapping
+    object UKAddress {
+      implicit val schema: Schema.CaseClass2[String, AbodeType, UKAddress] = DeriveSchema.gen[UKAddress]
+      val (postcode, abodeType)                                            = ProjectionExpression.accessors[UKAddress]
+    }
+    final case class USAddress(zip: String, abodeType: AbodeType) extends AddressDefaultMapping
+    object USAddress {
+      implicit val schema: Schema.CaseClass2[String, AbodeType, USAddress] = DeriveSchema.gen[USAddress]
+      val (zip, abodeType)                                                 = ProjectionExpression.accessors[USAddress]
+    }
+    implicit val schema: Schema.Enum2[UKAddress, USAddress, AddressDefaultMapping] =
+      DeriveSchema.gen[AddressDefaultMapping]
+    val (ukAddress, usAddress) = ProjectionExpression.accessors[AddressDefaultMapping]
+  }
+
+  final case class PersonDefault(id: String, surname: String, address: AddressDefaultMapping)
+  object PersonDefault {
+    implicit val schema: Schema.CaseClass3[String, String, AddressDefaultMapping, PersonDefault] =
+      DeriveSchema.gen[PersonDefault]
+    val (id, surname, address)                                                                   = ProjectionExpression.accessors[PersonDefault]
+  }
+
+  private val nestedSumTypeDefaultMappingSuite = suite("with default mapping")(
+    test("put and get person with UK address") {
+      val ukPerson = PersonDefault("1", "Smith", AddressDefaultMapping.UKAddress("DE23 7QW", AbodeType.House))
+      withSingleIdKeyTable { personTable =>
+        for {
+          _      <- put(personTable, ukPerson).execute
+          person <- get(personTable)(PersonDefault.id.partitionKey === "1").execute.absolve
+        } yield assertTrue(person == ukPerson)
+      }
+    },
+    test("put and get person with UU address") {
+      val usPerson = PersonDefault("1", "Smith", AddressDefaultMapping.USAddress("12345", AbodeType.House))
+      withSingleIdKeyTable { personTable =>
+        for {
+          _      <- put(personTable, usPerson).execute
+          person <- get(personTable)(PersonDefault.id.partitionKey === "1").execute.absolve
+        } yield assertTrue(person == usPerson)
+      }
+    }
+  )
+
+  @discriminatorName("addressType")
+  sealed trait AddressDiscriminatorName {
+    def abodeType: AbodeType
+  }
+
+  object AddressDiscriminatorName {
+    final case class UKAddress(postcode: String, abodeType: AbodeType) extends AddressDiscriminatorName
+    object UKAddress {
+      implicit val schema: Schema.CaseClass2[String, AbodeType, UKAddress] = DeriveSchema.gen[UKAddress]
+      val (postcode, abodeType)                                            = ProjectionExpression.accessors[UKAddress]
+    }
+    final case class USAddress(zip: String, abodeType: AbodeType) extends AddressDiscriminatorName
+    object USAddress {
+      implicit val schema: Schema.CaseClass2[String, AbodeType, USAddress] = DeriveSchema.gen[USAddress]
+      val (zip, abodeType)                                                 = ProjectionExpression.accessors[USAddress]
+    }
+    implicit val schema: Schema.Enum2[UKAddress, USAddress, AddressDiscriminatorName] =
+      DeriveSchema.gen[AddressDiscriminatorName]
+    val (ukAddress, usAddress) = ProjectionExpression.accessors[AddressDiscriminatorName]
+  }
+
+  final case class PersonDiscriminatorName(id: String, surname: String, address: AddressDiscriminatorName)
+  object PersonDiscriminatorName {
+    implicit val schema: Schema.CaseClass3[String, String, AddressDiscriminatorName, PersonDiscriminatorName] =
+      DeriveSchema.gen[PersonDiscriminatorName]
+    val (id, surname, address)                                                                                = ProjectionExpression.accessors[PersonDiscriminatorName]
+  }
+
+  private val nestedSumTypeDisriminatorNameMappingSuite = suite("with @disriminatorName annotation")(
+    test("put and get person with UK address") {
+      val ukPerson =
+        PersonDiscriminatorName("1", "Smith", AddressDiscriminatorName.UKAddress("DE23 7QW", AbodeType.House))
+      withSingleIdKeyTable { personTable =>
+        for {
+          _      <- put(personTable, ukPerson).execute
+          person <- get(personTable)(PersonDiscriminatorName.id.partitionKey === "1").execute.absolve
+        } yield assertTrue(person == ukPerson)
+      }
+    },
+    test("put and get person with US address") {
+      val usPerson = PersonDiscriminatorName("1", "Smith", AddressDiscriminatorName.USAddress("12345", AbodeType.House))
+      withSingleIdKeyTable { personTable =>
+        for {
+          _      <- put(personTable, usPerson).execute
+          person <- get(personTable)(PersonDiscriminatorName.id.partitionKey === "1").execute.absolve
+        } yield assertTrue(person == usPerson)
+      }
+    }
+  )
+
+  // WARNING - this annotation is only meant for integrating with legacy schemas - do not use green field work!
+  @noDiscriminator
+  sealed trait AddressNoDiscriminator {
+    def abodeType: AbodeType
+  }
+  object AddressNoDiscriminator       {
+    final case class UKAddress(postcode: String, abodeType: AbodeType) extends AddressNoDiscriminator
+    object UKAddress {
+      implicit val schema: Schema.CaseClass2[String, AbodeType, UKAddress] = DeriveSchema.gen[UKAddress]
+      val postcode                                                         = ProjectionExpression.accessors[UKAddress]
+    }
+    final case class USAddress(zip: String, abodeType: AbodeType) extends AddressNoDiscriminator
+    object USAddress {
+      implicit val schema: Schema.CaseClass2[String, AbodeType, USAddress] = DeriveSchema.gen[USAddress]
+      val zip                                                              = ProjectionExpression.accessors[USAddress]
+    }
+    implicit val schema: Schema.Enum2[UKAddress, USAddress, AddressNoDiscriminator] =
+      DeriveSchema.gen[AddressNoDiscriminator]
+    val (ukAddress, usAddress) = ProjectionExpression.accessors[AddressNoDiscriminator]
+  }
+
+  final case class PersonNoDiscriminator(id: String, surname: String, address: AddressNoDiscriminator)
+  object PersonNoDiscriminator {
+    implicit val schema: Schema.CaseClass3[String, String, AddressNoDiscriminator, PersonNoDiscriminator] =
+      DeriveSchema.gen[PersonNoDiscriminator]
+    val (id, surname, address)                                                                            = ProjectionExpression.accessors[PersonNoDiscriminator]
+  }
+
+  private val nestedSumTypeNoDisriminatorNameMappingSuite = suite("with @noDiscrimator annotation")(
+    test("put and get person with UK address") {
+      val ukPerson =
+        PersonNoDiscriminator("1", "Smith", AddressNoDiscriminator.UKAddress("DE23 7QW", AbodeType.House))
+      withSingleIdKeyTable { personTable =>
+        for {
+          _      <- put(personTable, ukPerson).execute
+          person <- get(personTable)(PersonNoDiscriminator.id.partitionKey === "1").execute.absolve
+        } yield assertTrue(person == ukPerson)
+      }
+    },
+    test("put and get person with US address") {
+      val usPerson = PersonNoDiscriminator("1", "Smith", AddressNoDiscriminator.USAddress("12345", AbodeType.House))
+      withSingleIdKeyTable { personTable =>
+        for {
+          _      <- put(personTable, usPerson).execute
+          person <- get(personTable)(PersonNoDiscriminator.id.partitionKey === "1").execute.absolve
+        } yield assertTrue(person == usPerson)
+      }
+    }
+  )
+
+  private val nestedSumTypeSuite = suite("Nested sum type suite")(
+    nestedSumTypeDefaultMappingSuite,
+    nestedSumTypeDisriminatorNameMappingSuite,
+    nestedSumTypeNoDisriminatorNameMappingSuite
   )
 }
