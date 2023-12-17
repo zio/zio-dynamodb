eff099f1166b525a716192cbffae68d994a13188
Avinder Bahra <avinder.bahra@gmail.com>
2023-12-17 08:03:22 +0000

    debug build problem

--- dynamodb/src/it/scala/zio/dynamodb/LiveSpec.scala
+++ dynamodb/src/it/scala/zio/dynamodb/LiveSpec.scala
@@ -629,7 +629,8 @@ object LiveSpec extends DynamoDBLocalSpec {
                 _      <- batchWriteFromStream(ZStream.fromIterable(1 to 10000).map(i => Item(id -> i))) { item =>
                             putItem(tableName, item)
                           }.runDrain
-                stream <- scanAllItem(tableName).parallel(8).execute
+                //stream <- scanAllItem(tableName).parallel(8).execute
+                stream <- scanAllItem(tableName).execute
                 count  <- stream.runFold(0) { case (count, _) => count + 1 }
               } yield assert(count)(equalTo(10000))
           )
@@ -645,7 +646,8 @@ object LiveSpec extends DynamoDBLocalSpec {
                   ) { item =>
                     putItem(tableName, item)
                   }.runDrain
-                stream <- scanAll[Person](tableName).parallel(8).execute
+                //stream <- scanAll[Person](tableName).parallel(8).execute
+                stream <- scanAll[Person](tableName).execute
                 count  <- stream.runFold(0) { case (count, _) => count + 1 }
               } yield assert(count)(equalTo(10000))
           )
