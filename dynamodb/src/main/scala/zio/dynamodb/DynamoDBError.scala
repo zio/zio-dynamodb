72bb8c654b463574038e6d3ffb2f740df603e567
Avinder Bahra <avinder.bahra@gmail.com>
2024-01-16 08:36:17 +0000

    comments

--- dynamodb/src/main/scala/zio/dynamodb/DynamoDBError.scala
+++ dynamodb/src/main/scala/zio/dynamodb/DynamoDBError.scala
@@ -10,13 +10,16 @@ sealed trait DynamoDBError extends Throwable with NoStackTrace with Product with
 }
 
 object DynamoDBError {
+  // TODO: rename to ItemError
   sealed trait DynamoDBItemError extends DynamoDBError
 
+  // TODO: rename to ItemError
   object DynamoDBItemError {
     final case class ValueNotFound(message: String) extends DynamoDBItemError
     final case class DecodingError(message: String) extends DynamoDBItemError
   }
 
+  // TODO: rename to AWSError
   final case class DynamoDBAWSError(cause: DynamoDbException) extends DynamoDBError {
     override def message: String = cause.getMessage
   }
