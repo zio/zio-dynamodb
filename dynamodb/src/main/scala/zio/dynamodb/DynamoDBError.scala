98bb1af8f71c3cbe3804de1afa598463288bb75b
Avinder Bahra <avinder.bahra@gmail.com>
2024-01-17 17:48:09 +0000

    improve error labels

--- dynamodb/src/main/scala/zio/dynamodb/DynamoDBError.scala
+++ dynamodb/src/main/scala/zio/dynamodb/DynamoDBError.scala
@@ -38,12 +38,12 @@ object DynamoDBError {
 
     // TODO: rename to WriteError
     final case class BatchWriteError(unprocessedItems: Map[String, Chunk[Write]]) extends DynamoDBBatchError {
-      val message = "BatchWriteError: unprocessed items returned by aws" // TODO remove "BatchWriteError: " part
+      val message = "unprocessed items returned by aws"
     }
 
     // TODO: rename to GetError
     final case class BatchGetError(unprocessedKeys: Map[String, Set[PrimaryKey]]) extends DynamoDBBatchError {
-      val message = "BatchGetError: unprocessed keys returned by aws" // TODO: remove "BatchGetError: "
+      val message = "unprocessed keys returned by aws"
     }
   }
 
