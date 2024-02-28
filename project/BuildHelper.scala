bc8d958a4524eb674b94ec52a644c45f441393a5
Scala Steward <scala_steward@virtuslab.com>
2024-02-28 14:16:40 +0000

    Update scala-library, scala-reflect to 2.13.13

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import BuildInfoKeys._
 object BuildHelper {
   // Align with zio-schema since we have a deep dependency on it
   val Scala212                = "2.12.18"
-  val Scala213                = "2.13.8"
+  val Scala213                = "2.13.13"
   val Scala3                  = "3.3.0"
   private val SilencerVersion = "1.7.14"
 
