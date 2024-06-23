e50b1055a38cfb95f5cf0457c8f089ab8569034c
Avinder Bahra <avinder.bahra@gmail.com>
2024-06-23 08:11:38 +0100

    relax IsPrimaryKey proof to include optional fields

--- dynamodb/src/main/scala/zio/dynamodb/proofs/IsPrimaryKey.scala
+++ dynamodb/src/main/scala/zio/dynamodb/proofs/IsPrimaryKey.scala
@@ -1,9 +1,10 @@
 package zio.dynamodb.proofs
 
 import scala.annotation.implicitNotFound
-import zio.dynamodb.KeyConditionExpr
 
-@implicitNotFound("DynamoDB does not support primary key type ${A} - allowed types are: String, Number, Binary")
+@implicitNotFound(
+  "DynamoDB does not support primary key type ${A} - allowed types are: String, Number, Binary and Options of those types"
+)
 sealed trait IsPrimaryKey[-A]
 
 object IsPrimaryKey {
@@ -15,9 +16,17 @@ object IsPrimaryKey {
   implicit val floatIsPrimaryKey: IsPrimaryKey[Float]           = new IsPrimaryKey[Float] {}
   implicit val doubleIsPrimaryKey: IsPrimaryKey[Double]         = new IsPrimaryKey[Double] {}
   implicit val bigDecimalIsPrimaryKey: IsPrimaryKey[BigDecimal] = new IsPrimaryKey[BigDecimal] {}
-
   implicit val binaryIsPrimaryKey: IsPrimaryKey[Iterable[Byte]] = new IsPrimaryKey[Iterable[Byte]] {}
 
-  implicit val sortKeyNotUsed: IsPrimaryKey[KeyConditionExpr.SortKeyNotUsed] =
-    new IsPrimaryKey[KeyConditionExpr.SortKeyNotUsed] {}
+  implicit val stringIsPrimaryKeyOpt: IsPrimaryKey[Option[String]]         = new IsPrimaryKey[Option[String]] {}
+  implicit val shortIsPrimaryKeyOpt: IsPrimaryKey[Option[Short]]           = new IsPrimaryKey[Option[Short]] {}
+  implicit val intIsPrimaryKeyOpt: IsPrimaryKey[Option[Int]]               = new IsPrimaryKey[Option[Int]] {}
+  implicit val longIsPrimaryKeyOpt: IsPrimaryKey[Option[Long]]             = new IsPrimaryKey[Option[Long]] {}
+  implicit val floatIsPrimaryKeyOpt: IsPrimaryKey[Option[Float]]           = new IsPrimaryKey[Option[Float]] {}
+  implicit val doubleIsPrimaryKeyOpt: IsPrimaryKey[Option[Double]]         = new IsPrimaryKey[Option[Double]] {}
+  implicit val bigDecimalIsPrimaryKeyOpt: IsPrimaryKey[Option[BigDecimal]] =
+    new IsPrimaryKey[Option[BigDecimal]] {}
+  implicit val binaryIsPrimaryKeyOpt: IsPrimaryKey[Option[Iterable[Byte]]] =
+    new IsPrimaryKey[Option[Iterable[Byte]]] {}
+
 }
