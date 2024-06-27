e4c3aba7537997d6b40a680c0a211170f59c69b9
Avinder Bahra <avinder.bahra@gmail.com>
2024-06-27 14:03:10 +0100

    fix restrictions on partition and sort key expressions for complex cases (#444)

--- examples/src/main/scala/zio/dynamodb/examples/KeyConditionExprExample.scala
+++ examples/src/main/scala/zio/dynamodb/examples/KeyConditionExprExample.scala
@@ -4,6 +4,7 @@ import zio.dynamodb.ProjectionExpression
 
 import zio.schema.Schema
 import zio.schema.DeriveSchema
+import zio.schema.annotation.discriminatorName
 import zio.dynamodb.DynamoDBQuery
 
 object KeyConditionExprExample extends App {
@@ -51,10 +52,67 @@ object KeyConditionExprExample extends App {
   val pkAndSkExtended5 =
     Student.email.partitionKey === "x" && Student.binary.sortKey.beginsWith(List(1.toByte))
   val pkAndSkExtended6 =
-    Student.email.partitionKey === "x" && Student.binary2.sortKey.beginsWith(List(1.toByte))
+    Student.email.partitionKey === "x" && Student.binary2.sortKey.beginsWith(Vector(1.toByte))
 
   val (aliasMap, s) = pkAndSkExtended1.render.execute
   println(s"aliasMap=$aliasMap, s=$s")
 
   val get = DynamoDBQuery.queryAllItem("table").whereKey($("foo.bar").partitionKey === 1 && $("foo.baz").sortKey > 1)
+
+  // more complex example
+  @discriminatorName("invoiceType")
+  sealed trait InvoiceWithDiscriminatorName {
+    def id: String
+  }
+  object InvoiceWithDiscriminatorName       {
+    final case class Unpaid(id: String) extends InvoiceWithDiscriminatorName
+    object Unpaid {
+      implicit val schema: Schema.CaseClass1[String, Unpaid] = DeriveSchema.gen[Unpaid]
+      val id                                                 = ProjectionExpression.accessors[Unpaid]
+    }
+    final case class Paid(
+      id: String,
+      amount: Option[Int],
+      accountId: Option[String] = None,
+      email: Option[String] = None
+    ) extends InvoiceWithDiscriminatorName
+    object Paid   {
+      implicit val schema: Schema.CaseClass4[String, Option[Int], Option[String], Option[String], Paid] =
+        DeriveSchema.gen[Paid]
+      val (id, amount, accountId, email)                                                                = ProjectionExpression.accessors[Paid]
+    }
+    implicit val schema: Schema.Enum2[Unpaid, Paid, InvoiceWithDiscriminatorName] =
+      DeriveSchema.gen[InvoiceWithDiscriminatorName]
+    val (unpaid, paid) = ProjectionExpression.accessors[InvoiceWithDiscriminatorName]
+  }
+
+  val peId: ProjectionExpression[InvoiceWithDiscriminatorName, String]                =
+    InvoiceWithDiscriminatorName.paid >>> InvoiceWithDiscriminatorName.Paid.id
+  val peAccountId: ProjectionExpression[InvoiceWithDiscriminatorName, Option[String]] =
+    InvoiceWithDiscriminatorName.paid >>> InvoiceWithDiscriminatorName.Paid.accountId
+  val peEmailId: ProjectionExpression[InvoiceWithDiscriminatorName, Option[String]]   =
+    InvoiceWithDiscriminatorName.paid >>> InvoiceWithDiscriminatorName.Paid.email
+  val peAmount: ProjectionExpression[InvoiceWithDiscriminatorName, Option[Int]]       =
+    InvoiceWithDiscriminatorName.paid >>> InvoiceWithDiscriminatorName.Paid.amount
+  val pk1: PartitionKeyEquals[InvoiceWithDiscriminatorName]                           = peId.partitionKey === "1"
+  val pk2: PartitionKeyEquals[InvoiceWithDiscriminatorName]                           = peAccountId.partitionKey === Some("1")
+  val pk3: CompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]                      =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey === Some("1")
+  val pk4: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]              =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey > Some("1")
+  val pk5: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]              =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey < Some("1")
+  val pk6: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]              =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey <> Some("1")
+  val pk7: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]              =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey <= Some("1")
+  val pk8: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]              =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey >= Some("1")
+  val pk9: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]              =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey.between(Some("1"), Some("3"))
+  val pk10: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]             =
+    peAccountId.partitionKey === Some("1") && peEmailId.sortKey.beginsWith(Some("1"))
+  // val pk11: ExtendedCompositePrimaryKeyExpr[InvoiceWithDiscriminatorName]             =
+  //   peAccountId.partitionKey === Some("1") && peAmount.sortKey.beginsWith(Some(1)) // does not compile as expected
+
 }
