e49fa513a1ab3f72e3a15cce8308fa8b8e56119f
Kyri Petrou <kyri.petrou@outlook.com>
2024-11-28 11:02:59 +0530

    Update dependencies

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -5,10 +5,10 @@ import BuildInfoKeys._
 
 object BuildHelper {
   // Align with zio-schema since we have a deep dependency on it
-  val Scala212                = "2.12.19"
-  val Scala213                = "2.13.12"
-  val Scala3                  = "3.3.1"
-  private val SilencerVersion = "1.7.17"
+  val Scala212                = "2.12.20"
+  val Scala213                = "2.13.15"
+  val Scala3                  = "3.3.4"
+  private val SilencerVersion = "1.7.19"
 
   private val stdOptions = Seq(
     "-encoding",
