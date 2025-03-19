1ec2f26661958cb3fda42aeb2d7bac77d41fb371
Avinder Bahra <avinder.bahra@gmail.com>
2025-03-19 06:17:07 +0000

    some cleanup

--- benchmarks/src/main/scala/zio/dynamodb/benchmarks/RoundTripBenchmark.scala
+++ benchmarks/src/main/scala/zio/dynamodb/benchmarks/RoundTripBenchmark.scala
@@ -2,11 +2,8 @@ package zio.dynamodb.benchmarks
 
 import org.openjdk.jmh.annotations._
 import zio.dynamodb._
-//import zio.{ Scope => _, _ }
 
 import java.util.concurrent.TimeUnit
-import zio.schema.DeriveSchema
-import zio.schema.Schema
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
@@ -15,35 +12,26 @@ import zio.schema.Schema
 @Warmup(iterations = 15)
 @Fork(2)
 class RoundTripBenchmark {
-  import RoundTripBenchmark._
 
   @Param(Array("500"))
   var repetitions: Int = _
 
   @Setup
   def setup(): Unit =
-    zioDynamoDB = ZIODynamoDbBenchmark.unsafeMake()
+    zioDdbBenchmark = ZIODynamoDbBenchmark.unsafeMake()
 
   @TearDown
   def tearDown(): Unit =
-    zioDynamoDB.tearDown()
+    zioDdbBenchmark.tearDown()
 
   @Benchmark
-  def zioDDB(): Unit =
-    zioDynamoDB.run {
+  def zioDdbApiBenchmark(): Unit =
+    zioDdbBenchmark.run {
       for {
         _ <- DynamoDBQuery.put("Person", Person("1", "John")).execute
         _ <- DynamoDBQuery.get("Person")(Person.id.partitionKey === "1").execute
       } yield ()
     }
 
-  private var zioDynamoDB: ZIODynamoDbBenchmark = _
-}
-
-object RoundTripBenchmark {
-  final case class Person(id: String, name: String)
-  object Person {
-    implicit val schema: Schema.CaseClass2[String, String, Person] = DeriveSchema.gen[Person]
-    val (id, name)                                                 = ProjectionExpression.accessors[Person]
-  }
+  private var zioDdbBenchmark: ZIODynamoDbBenchmark = _
 }
--- benchmarks/src/main/scala/zio/dynamodb/benchmarks/ZIODynamoDbBenchmark.scala
+++ benchmarks/src/main/scala/zio/dynamodb/benchmarks/ZIODynamoDbBenchmark.scala
@@ -7,15 +7,20 @@ import zio.Unsafe
 import zio.ZIO
 import zio.dynamodb.DynamoDBError
 
-private[benchmarks] final class ZIODynamoDbBenchmark private (runtime: Runtime.Scoped[TestDynamoDBExecutor with DynamoDBExecutor]) {
+private[benchmarks] final class ZIODynamoDbBenchmark private (
+  runtime: Runtime.Scoped[TestDynamoDBExecutor with DynamoDBExecutor]
+) {
+
   def run(program: ZIO[TestDynamoDBExecutor with DynamoDBExecutor, DynamoDBError, Unit]): Unit =
     Unsafe.unsafe(implicit unsafe => runtime.unsafe.run(program).getOrThrow())
 
-  def tearDown(): Unit = Unsafe.unsafe(implicit u => runtime.unsafe.shutdown())
+  def tearDown(): Unit = Unsafe.unsafe(implicit unsafe => runtime.unsafe.shutdown())
 }
-object ZIODynamoDbBenchmark                                                                              {
+object ZIODynamoDbBenchmark {
   def unsafeMake(): ZIODynamoDbBenchmark = {
-    val runtime = Unsafe.unsafe(implicit unsafe => Runtime.unsafe.fromLayer(DynamoDBExecutor.test("Person" -> "id")))
+    val runtime = Unsafe.unsafe(implicit unsafe =>
+      Runtime.unsafe.fromLayer(DynamoDBExecutor.test(Person.tableName -> Person.idColumnName))
+    )
     new ZIODynamoDbBenchmark(runtime)
   }
 }
