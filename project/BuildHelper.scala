5513075c8ed4aeb3bdf7a541eec6c5a1bd66a93a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-05-18 07:37:03 +0200

    Update scala3-compiler, scala3-library, ... to 3.3.6 (#667)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ object BuildHelper {
   // Align with zio-schema since we have a deep dependency on it
   val Scala212                = "2.12.20"
   val Scala213                = "2.13.16"
-  val Scala3                  = "3.3.5"
+  val Scala3                  = "3.3.6"
   private val SilencerVersion = "1.7.19"
 
   private val stdOptions = Seq(
