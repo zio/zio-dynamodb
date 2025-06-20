1517c6572edf588ca12b6ecf6de2f8e273ae703b
Avinder Bahra <avinder.bahra@gmail.com>
2025-06-20 07:48:57 +0100

    remove package object

--- interop/dynamodb-future/src/main/scala/zio/dynamodb/interop/future/syntax.scala
+++ interop/dynamodb-future/src/main/scala/zio/dynamodb/interop/future/syntax.scala
@@ -2,7 +2,7 @@ package zio.dynamodb.interop.future
 
 import zio.CancelableFuture
 
-package object syntax {
+object syntax {
   implicit class ZioDynamoDBQueryOps[In, Out](val query: zio.dynamodb.DynamoDBQuery[In, Out]) extends AnyVal {
     def executeToF(implicit ev: DynamoDBExecutorF): CancelableFuture[Out] =
       ev.execute(query)
