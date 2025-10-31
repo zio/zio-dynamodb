27089e24a2bef38704ad8a5f8da4bef0292ebdfa
Avinder Bahra <avinder.bahra@gmail.com>
2025-10-31 09:41:48 +0000

    delete dead code

--- dynamodb/src/main/scala/zio/dynamodb/AttributeValue.scala
+++ dynamodb/src/main/scala/zio/dynamodb/AttributeValue.scala
@@ -58,8 +58,6 @@ object AttributeValue {
   import Predef.{ String => ScalaString }
   import scala.collection.immutable.{ Map => ScalaMap }
 
-  def single(fieldName: ScalaString, av: AttributeValue): Map = Map(ScalaMap(String(fieldName) -> av))
-
   type WithScalaType[X] = AttributeValue { type ScalaType = X }
 
   private[dynamodb] final case class Binary(value: Iterable[Byte])              extends AttributeValue
