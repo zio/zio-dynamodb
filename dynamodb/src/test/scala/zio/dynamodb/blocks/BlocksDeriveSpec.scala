b99974bbf74ee738b259e20f888d7944cf299193
Avinder Bahra <avinder.bahra@gmail.com>
2025-11-15 07:32:08 +0000

    improve tuple test

--- dynamodb/src/test/scala/zio/dynamodb/blocks/BlocksDeriveSpec.scala
+++ dynamodb/src/test/scala/zio/dynamodb/blocks/BlocksDeriveSpec.scala
@@ -101,7 +101,7 @@ object BlocksDeriveSpec extends ZIOSpecDefault {
     implicit val schema: Schema[RecordWithOption] = Schema.derived
   }
 
-  final case class RecordWithTuple(id: String, tuple: (String, Int))
+  final case class RecordWithTuple(tuple: (String, Int))
   object RecordWithTuple {
     implicit val schema: Schema[RecordWithTuple] = Schema.derived
   }
@@ -232,9 +232,9 @@ object BlocksDeriveSpec extends ZIOSpecDefault {
     // TODO: Avi - implement Tuple codec
     test("Record with tuple") {
       val expectedItem                     =
-        Item("id" -> "1", "tuple" -> Item("_1" -> "a", "_2" -> 2))
+        Item("tuple" -> Item("_1" -> "a", "_2" -> 2))
       val codec: DdbCodec[RecordWithTuple] = RecordWithTuple.schema.derive(BlocksDdbDerived)
-      val expected                         = RecordWithTuple("1", ("a", 2))
+      val expected                         = RecordWithTuple(("a", 2))
       val enc                              = codec.encoder(expected)
       val dec                              = codec.decoder(enc)
       assertTrue(enc == expectedItem.toAttributeValue && dec == Right(expected))
