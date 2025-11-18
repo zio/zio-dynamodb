6034fb49ace15af86fe959695ac6cc1882ccb3d9
Avinder Bahra <avinder.bahra@gmail.com>
2025-11-18 08:08:18 +0000

    cleanup

--- dynamodb/src/main/scala/zio/dynamodb/blocks/BlocksDdbDerived2.scala
+++ dynamodb/src/main/scala/zio/dynamodb/blocks/BlocksDdbDerived2.scala
@@ -207,7 +207,6 @@ object BlocksDdbDerived2 extends Deriver[DdbCodec] { self =>
 
   private val intCodec: DdbCodec[Int] = new DdbCodec[Int] {
     override def encoder: Encoder[Int] =
-//      (a: Int) => AttributeValue.Number(BigDecimal(a.toString))
       (a: Int) => AttributeValue.Number(BigDecimal.valueOf(a.toLong))
 
     override def decoder: Decoder[Int] =
@@ -226,7 +225,6 @@ object BlocksDdbDerived2 extends Deriver[DdbCodec] { self =>
   }
   private val longCodec                     = new DdbCodec[Long] {
     override def encoder: Encoder[Long] =
-      //(a: Long) => AttributeValue.Number(BigDecimal(a.toString))
       (a: Long) => AttributeValue.Number(BigDecimal.valueOf(a))
 
     override def decoder: Decoder[Long] =
