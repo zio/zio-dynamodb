fd1606401f343b260456eac05e9dd9ff07b41623
Avinder Bahra <avinder.bahra@gmail.com>
2026-03-02 07:52:12 +0000

    fix missing Schema implicit. improve grammar

--- dynamodb/src/test/scala-3/zio/dynamodb/blocks/Scala3AllowsSpec.scala
+++ dynamodb/src/test/scala-3/zio/dynamodb/blocks/Scala3AllowsSpec.scala
@@ -6,7 +6,6 @@ import zio.dynamodb.{
   blocks,
   AttributeValue,
   ProjectionExpression,
-  ProjectionExpressionOps,
   ToAttributeValue,
   UpdateExpression
 }
@@ -35,17 +34,25 @@ object Scala3AllowsSpec extends ZIOSpecDefault {
   }
   type Age = Age.Type
 
+  case class Address(number: String, postcode: String)
+  object Address extends CompanionOptics[Address] {
+    implicit val schema: Schema[Address] = Schema.derived
+    val number: Optic[Address, String] = $(_.number)
+    val postcode: Optic[Address, String] = $(_.postcode)
+  }
+
   case class Person(
-    id: PersonId,
-    name: String,
-    age: Int,
-    opaqueInt: OpaqueId,
-    ageWrapped: Age,
-    setInt: Set[Int] = Set.empty,
-    setString: Set[String] = Set.empty,
-    setPersonId: Set[PersonId] = Set.empty,
-    map: Map[String, Int] = Map.empty,
-    list: List[Int] = Nil
+                     id: PersonId,
+                     name: String,
+                     age: Int,
+                     opaqueInt: OpaqueId,
+                     ageWrapped: Age,
+                     setInt: Set[Int] = Set.empty,
+                     setString: Set[String] = Set.empty,
+                     setPersonId: Set[PersonId] = Set.empty,
+                     map: Map[String, Int] = Map.empty,
+                     listInt: List[Int] = Nil,
+                     listAddress: List[Address] = Nil
   )
   object Person extends CompanionOptics[Person] {
     implicit val schema: Schema[Person]           = Schema.derived
@@ -58,7 +65,8 @@ object Scala3AllowsSpec extends ZIOSpecDefault {
     val setString: Optic[Person, Set[String]]     = $(_.setString)
     val setPersonId: Optic[Person, Set[PersonId]] = $(_.setPersonId)
     val map: Optic[Person, Map[String, Int]]      = $(_.map)
-    val list: Optic[Person, List[Int]]            = $(_.list)
+    val listInt: Optic[Person, List[Int]]         = $(_.listInt)
+    val listAddress: Optic[Person, List[Address]]   = $(_.listAddress)
   }
 
   import ExtensionMethods._
@@ -73,7 +81,8 @@ object Scala3AllowsSpec extends ZIOSpecDefault {
         Person.setInt.addSet(Set(1))
         Person.setString.addSet(Set("hello"))
         Person.setPersonId.addSet(Set(PersonId(1)))
-        Person.list.remove(1)
+        Person.listInt.remove(1)
+        Person.listAddress.remove(1)
         Person.setInt.addSet(Set(1))
 
         assertTrue(true)
@@ -93,78 +102,92 @@ object Scala3AllowsSpec extends ZIOSpecDefault {
         )
       }
     )
-}
-object ExtensionMethods {
-  import zio.blocks.schema.comptime.Allows
-  import Allows._
 
-  type Numeric = Primitive.Int | Primitive.Long | Primitive.Float | Primitive.Double | Primitive.Short
+  object ExtensionMethods {
 
-  // we have no Set so use Sequence as an approximation for now
-  type NumberSet = Sequence[Numeric] | Sequence[Wrapped[Numeric]]
-  type StringSet = Sequence[Primitive.String] | Sequence[Wrapped[Primitive.String]]
-  type BinarySet = Sequence[Sequence[Primitive.Byte]]
-  type NativeSet = NumberSet | StringSet | BinarySet
+    import zio.blocks.schema.comptime.Allows
+    import Allows._
 
-  implicit class OpticToDdbExpr[From, To: ToAttributeValue](optic: Optic[From, To]) {
-    private def self: ProjectionExpression[From, To] = OpticToPE.pe(optic)
+    // scalars
+    type N = Primitive.Int | Primitive.Long | Primitive.Float | Primitive.Double | Primitive.Short
+    type S = Primitive.String
+    type BOOL = Primitive.Boolean
 
-    /*
-ADD update behaviour
-| Attribute Type    | Allowed? | Behaviour         |
-| ----------------- | -------- | ----------------- |
-| `N` (Number)      | ✅        | Numeric increment |
-| `NS` (Number Set) | ✅        | Set union         |
-| `SS` (String Set) | ✅        | Set union         |
-| `BS` (Binary Set) | ✅        | Set union         |
-| `S` (String)      | ❌        | Not allowed       |
-| `L` (List)        | ❌        | Not allowed       |
-| `M` (Map)         | ❌        | Not allowed       |
-| `BOOL`            | ❌        | Not allowed       |
-| `NULL`            | ❌        | Not allowed       |
-     */
+    // DynamoDb native collections
+    type NativeMap = Map[Primitive.String, All]
+    // we have no Set so use Sequence as an approximation for now
+    type NS = Sequence[N] | Sequence[Wrapped[N]]
+    type SS = Sequence[Primitive.String] | Sequence[Wrapped[Primitive.String]]
+    type BS = Sequence[Sequence[Primitive.Byte]]
 
-    def add[A](a: A)(implicit
-      ev: Allows[A, Numeric | Wrapped[Numeric]],
-      ev2: Allows[To, Numeric | Wrapped[Numeric]],
-      to: ToAttributeValue[A]
-    ): UpdateExpression.Action.AddAction[From] =
-      UpdateExpression.Action.AddAction(
-        self,
-        to.toAttributeValue(a)
-      )
+    //type NULL = Primitive.Unit ????? // I think we can ignore this in the world of Scala types
 
-    def addSet[A](
-      set: Set[A]
-    )(implicit
-      ev: Allows[To, NativeSet],
-      evSet: Set[A] <:< To
-    ): UpdateExpression.Action.AddAction[From] =
-      UpdateExpression.Action.AddAction(
-        self,
-        ToAttributeValue[To].toAttributeValue(evSet(set))
-      )
+    type DdbRecord = Record[N | S | BOOL | NS | SS | BS | Self]
+    type L = Sequence[All]
 
-    /*
-Remove at index UpdateExpression behaviour
-| Attribute Type | Allowed? |
-| -------------- | -------- |
-| `L` (List)     | ✅        |
-| `SS`           | ❌        |
-| `NS`           | ❌        |
-| `BS`           | ❌        |
-| `N`            | ❌        |
-| `S`            | ❌        |
-| `M`            | ❌        |
-     */
-    def remove(
-      index: Int
-    )(implicit ev: Allows[To, Sequence[Numeric]]): UpdateExpression.Action.RemoveAction[From] =
-      UpdateExpression.Action.RemoveAction(ProjectionExpression.ListElement(self, index))
+    type All = N | NS | SS | BS | S | BOOL | DdbRecord | Sequence[Self] | Map[Self, Self]
 
-    def removeOld[From2 <: From](
-      index: Int
-    )(implicit ev: ListRemoveable[To]): UpdateExpression.Action.RemoveAction[From2] =
-      UpdateExpression.Action.RemoveAction(ProjectionExpression.ListElement(self, index))
+
+    implicit class OpticToDdbExpr[From, To: ToAttributeValue](optic: Optic[From, To]) {
+      private def self: ProjectionExpression[From, To] = OpticToPE.pe(optic)
+
+      /*
+  ADD update behaviour
+  | Attribute Type    | Allowed? | Behaviour         |
+  | ----------------- | -------- | ----------------- |
+  | `N` (Number)      | ✅        | Numeric increment |
+  | `NS` (Number Set) | ✅        | Set union         |
+  | `SS` (String Set) | ✅        | Set union         |
+  | `BS` (Binary Set) | ✅        | Set union         |
+  | `S` (String)      | ❌        | Not allowed       |
+  | `L` (List)        | ❌        | Not allowed       |
+  | `M` (Map)         | ❌        | Not allowed       |
+  | `BOOL`            | ❌        | Not allowed       |
+  | `NULL`            | ❌        | Not allowed       |
+       */
+
+      def add[A](a: A)(implicit
+                       ev: Allows[A, N | Wrapped[N]],
+                       ev2: Allows[To, N | Wrapped[N]],
+                       to: ToAttributeValue[A]
+      ): UpdateExpression.Action.AddAction[From] =
+        UpdateExpression.Action.AddAction(
+          self,
+          to.toAttributeValue(a)
+        )
+
+      def addSet[A](
+                     set: Set[A]
+                   )(implicit
+                     ev: Allows[To, NS | SS | BS],
+                     evSet: Set[A] <:< To
+                   ): UpdateExpression.Action.AddAction[From] =
+        UpdateExpression.Action.AddAction(
+          self,
+          ToAttributeValue[To].toAttributeValue(evSet(set))
+        )
+
+      /*
+  Remove at index UpdateExpression behaviour
+  | Attribute Type | Allowed? |
+  | -------------- | -------- |
+  | `L` (List)     | ✅        |
+  | `SS`           | ❌        |
+  | `NS`           | ❌        |
+  | `BS`           | ❌        |
+  | `N`            | ❌        |
+  | `S`            | ❌        |
+  | `M`            | ❌        |
+       */
+      def remove(
+                  index: Int
+                )(implicit ev: Allows[To, L]): UpdateExpression.Action.RemoveAction[From] =
+        UpdateExpression.Action.RemoveAction(ProjectionExpression.ListElement(self, index))
+
+      def removeOld[From2 <: From](
+                                    index: Int
+                                  )(implicit ev: ListRemoveable[To]): UpdateExpression.Action.RemoveAction[From2] =
+        UpdateExpression.Action.RemoveAction(ProjectionExpression.ListElement(self, index))
+    }
   }
 }
