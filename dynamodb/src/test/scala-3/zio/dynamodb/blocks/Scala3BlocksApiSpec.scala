f8abe059c04d486fecffa98d82541aaa06dd80c0
Avinder Bahra <avinder.bahra@gmail.com>
2026-03-14 08:33:13 +0000

    cleanup and nested variants

--- dynamodb/src/test/scala-3/zio/dynamodb/blocks/Scala3BlocksApiSpec.scala
+++ dynamodb/src/test/scala-3/zio/dynamodb/blocks/Scala3BlocksApiSpec.scala
@@ -68,7 +68,6 @@ object Scala3BlocksApiSpec extends ZIOSpecDefault {
     case End
   }
 
-  // Add the roundTripWithSchema2Codec definition here for test usage
   private def roundTripWithSchema2Codec[A](
     expectedValue: A,
     expectedAV: AttributeValue,
@@ -146,51 +145,148 @@ object Scala3BlocksApiSpec extends ZIOSpecDefault {
               )
             )
           )(schema2)
-        } @@ TestAspect.ignore // TODO: Avi - implement recursive cache in codec
+        } @@ TestAspect.ignore, // TODO: Avi - ignore until recursive cache in codec is implemented
+        test("Person with simple enum") {
+          roundTripWithSchema2Codec(
+            expectedValue = Person(
+              id = 123,
+              name = "John Doe",
+              trafficLight = TrafficLight.Green
+            ),
+            expectedAV = Item("id" -> OpaqueId(123), "name" -> "John Doe", "trafficLight" -> "Green").toAttributeValue
+          )
+        },
+        test("Complex enum PaymentMethod.PayPal with no discriminator") {
+          roundTripWithSchema2Codec(
+            expectedValue = PaymentMethod.PayPal("a@b.com"),
+            expectedAV = Item("PayPal" -> Item("email" -> "a@b.com")).toAttributeValue
+          )
+        },
+        test("Complex enum PaymentMethod.Other with no discriminator") {
+          roundTripWithSchema2Codec(
+            expectedValue = PaymentMethod.Other,
+            expectedAV = Item("Other" -> Item.empty).toAttributeValue
+          )
+        },
+        test("Complex enum PaymentMethod.PayPal with discriminator field foo") {
+          roundTripWithSchema2Codec(
+            expectedValue = PaymentMethod.PayPal("a@b.com"),
+            expectedAV = Item("email" -> "a@b.com", "foo" -> "PayPal").toAttributeValue,
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.Field("foo"))
+          )
+        },
+        test("union type with key discriminator") {
+          type Value = Int | String | (Int, String) | List[Int]
+          val schema = Schema.derived[Value]
+
+          roundTripWithSchema2Codec(
+            expectedValue = "foo",
+            expectedAV = Item("java.lang.String" -> "foo").toAttributeValue
+          )(schema) &&
+          roundTripWithSchema2Codec(
+            expectedValue = (1, "foo"),
+            expectedAV = AttributeValue.Map(
+              Map(
+                AttributeValue.String("scala.Tuple2") ->
+                  AttributeValue.List(
+                    List(
+                      AttributeValue.Number(BigDecimal(1)),
+                      AttributeValue.String("foo")
+                    )
+                  )
+              )
+            )
+          )(schema) &&
+          roundTripWithSchema2Codec(
+            expectedValue = List(1, 2),
+            expectedAV = AttributeValue.Map(
+              Map(
+                AttributeValue.String("scala.collection.immutable.List") ->
+                  AttributeValue.List(List(AttributeValue.Number(BigDecimal(1)), AttributeValue.Number(BigDecimal(2))))
+              )
+            )
+          )(schema)
+        },
+        test("union type without discriminator") {
+          type Value = Int | String | (Int, String) | List[Int]
+          val schema = Schema.derived[Value]
+
+          roundTripWithSchema2Codec[Value](
+            expectedValue = "foo",
+            expectedAV = AttributeValue.String("foo"),
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
+          )(schema) &&
+          roundTripWithSchema2Codec[Value](
+            expectedValue = (1, "foo"),
+            expectedAV = AttributeValue.List(
+              List(
+                AttributeValue.Number(BigDecimal(1)),
+                AttributeValue.String("foo")
+              )
+            ),
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
+          )(schema) &&
+          roundTripWithSchema2Codec[Value](
+            expectedValue = List(1, 2),
+            expectedAV =
+              AttributeValue.List(List(AttributeValue.Number(BigDecimal(1)), AttributeValue.Number(BigDecimal(2)))),
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None).withSchema1TupleCompatibility(false)
+          )(schema)
+        },
+        test("nested variants without discriminator") {
+          type Value = Int | String | (Int, String) | List[Int]
+
+          sealed trait Base derives Schema
+
+          case class Case1(value: Value) extends Base
+
+          case class Case2(value: Map[String, Long]) extends Base
+
+          roundTripWithSchema2Codec[Base](
+            expectedValue = Case1(1),
+            expectedAV = Item("value" -> 1).toAttributeValue,
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
+          ) &&
+          roundTripWithSchema2Codec[Base](
+            expectedValue = Case1("foo"),
+            expectedAV = Item("value" -> "foo").toAttributeValue,
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
+          ) &&
+          roundTripWithSchema2Codec[Base](
+            expectedValue = Case1((1, "foo")),
+            expectedAV = AttributeValue.Map(
+              Map(
+                AttributeValue.String("value") -> AttributeValue.List(
+                  List(
+                    AttributeValue.Number(BigDecimal(1)),
+                    AttributeValue.String("foo")
+                  )
+                )
+              )
+            ),
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
+          ) &&
+          roundTripWithSchema2Codec[Base](
+            expectedValue = Case1(List(1, 2)),
+            expectedAV = AttributeValue.Map(
+              Map(
+                AttributeValue.String("value") -> AttributeValue.List(
+                  List(
+                    AttributeValue.Number(BigDecimal(1)),
+                    AttributeValue.Number(BigDecimal(2))
+                  )
+                )
+              )
+            ),
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None).withSchema1TupleCompatibility(false)
+          ) &&
+          roundTripWithSchema2Codec[Base](
+            expectedValue = Case2(Map("foo" -> 1L)),
+            expectedAV = Item("value" -> Item("foo" -> 1L)).toAttributeValue,
+            deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
+          )
+        }
       ),
-      test("Person with simple enum") {
-        roundTripWithSchema2Codec(
-          expectedValue = Person(
-            id = 123,
-            name = "John Doe",
-            trafficLight = TrafficLight.Green
-          ),
-          expectedAV = Item("id" -> OpaqueId(123), "name" -> "John Doe", "trafficLight" -> "Green").toAttributeValue
-        )
-      },
-      test("simple enum Green") {
-        roundTripWithSchema2Codec(
-          expectedValue = TrafficLight.Green,
-          expectedAV = AttributeValue.String("Green")
-        ) &&
-        roundTripWithSchema2Codec(
-          expectedValue = TrafficLight.Yellow,
-          expectedAV = AttributeValue.String("Yellow")
-        ) &&
-        roundTripWithSchema2Codec(
-          expectedValue = TrafficLight.Red,
-          expectedAV = AttributeValue.String("Red")
-        )
-      },
-      test("Complex enum PaymentMethod.PayPal with no discriminator") {
-        roundTripWithSchema2Codec(
-          expectedValue = PaymentMethod.PayPal("a@b.com"),
-          expectedAV = Item("PayPal" -> Item("email" -> "a@b.com")).toAttributeValue
-        )
-      },
-      test("Complex enum PaymentMethod.Other with no discriminator") {
-        roundTripWithSchema2Codec(
-          expectedValue = PaymentMethod.Other,
-          expectedAV = Item("Other" -> Item.empty).toAttributeValue
-        )
-      },
-      test("Complex enum PaymentMethod.PayPal with discriminator field foo") {
-        roundTripWithSchema2Codec(
-          expectedValue = PaymentMethod.PayPal("a@b.com"),
-          expectedAV = Item("email" -> "a@b.com", "foo" -> "PayPal").toAttributeValue,
-          deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.Field("foo"))
-        )
-      },
       test("PersonId Prelude Newtype") {
         roundTripWithSchema2Codec(
           expectedValue = PersonId(1),
@@ -211,64 +307,6 @@ object Scala3BlocksApiSpec extends ZIOSpecDefault {
               AttributeValue.String("bar")
             )
           )
-        )(schema)
-      },
-      test("union type with key discriminator") {
-        type Value = Int | String | (Int, String) | List[Int]
-        val schema = Schema.derived[Value]
-
-        roundTripWithSchema2Codec(
-          expectedValue = "foo",
-          expectedAV = Item("java.lang.String" -> "foo").toAttributeValue
-        )(schema) &&
-        roundTripWithSchema2Codec(
-          expectedValue = (1, "foo"),
-          expectedAV = AttributeValue.Map(
-            Map(
-              AttributeValue.String("scala.Tuple2") ->
-                AttributeValue.List(
-                  List(
-                    AttributeValue.Number(BigDecimal(1)),
-                    AttributeValue.String("foo")
-                  )
-                )
-            )
-          )
-        )(schema) &&
-        roundTripWithSchema2Codec(
-          expectedValue = List(1, 2),
-          expectedAV = AttributeValue.Map(
-            Map(
-              AttributeValue.String("scala.collection.immutable.List") ->
-                AttributeValue.List(List(AttributeValue.Number(BigDecimal(1)), AttributeValue.Number(BigDecimal(2))))
-            )
-          )
-        )(schema)
-      },
-      test("union type without discriminator") {
-        type Value = Int | String | (Int, String) | List[Int]
-        val schema = Schema.derived[Value]
-
-        roundTripWithSchema2Codec[Value](
-          expectedValue = "foo",
-          expectedAV = AttributeValue.String("foo"),
-          deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
-        )(schema) &&
-        roundTripWithSchema2Codec[Value](
-          expectedValue = (1, "foo"),
-          expectedAV = AttributeValue.List(
-            List(
-              AttributeValue.Number(BigDecimal(1)),
-              AttributeValue.String("foo")
-            )
-          ),
-          deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None)
-        )(schema) &&
-        roundTripWithSchema2Codec[Value](
-          expectedValue = List(1, 2),
-          expectedAV =
-            AttributeValue.List(List(AttributeValue.Number(BigDecimal(1)), AttributeValue.Number(BigDecimal(2)))),
-          deriverConfigure = _.withDiscriminatorKind(DiscriminatorKind.None).withSchema1TupleCompatibility(false)
         )(schema)
       }
     )
