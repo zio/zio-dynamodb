2856edd01a74393433440eef4a39f1ea7172b710
Avinder Bahra <avinder.bahra@gmail.com>
2026-04-06 15:02:07 +0100

    introduce compatibility type alias for Allows |

---
+++ dynamodb/src/main/scala-3/zio/dynamodb/blocks/compat/package.scala
@@ -0,0 +1,8 @@
+package zio.dynamodb.blocks
+
+import zio.blocks.schema.comptime.Allows
+import Allows._
+
+package object compat {
+  type Or[A <: Allows.Structural, B <: Allows.Structural] = A | B
+}
