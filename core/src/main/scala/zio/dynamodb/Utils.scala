ab7be7ab571225080668e4d4fb78b00b4293a9de
Avinder Bahra <avinder.bahra@gmail.com>
2026-07-29 16:42:27 +0100

    Narrow ListUtils implicit class to its receiver type param
    E and B were unused on the class itself (only forEach needs them),
    which unnecessarily complicated implicit resolution/inference. Move
    them to forEach and make the class extend AnyVal now that it only
    carries A, avoiding a wrapper allocation on each use.

--- core/src/main/scala/zio/dynamodb/Utils.scala
+++ core/src/main/scala/zio/dynamodb/Utils.scala
@@ -18,8 +18,8 @@ package zio.dynamodb
 
 object Utils {
   // move Prelude ops here to maintain zero dependency
-  implicit class ListUtils[E, A, B](list: Iterable[A]) {
-    def forEach(f: A => Either[E, B]): Either[E, Iterable[B]] = {
+  implicit class ListUtils[A](val list: Iterable[A]) extends AnyVal {
+    def forEach[E, B](f: A => Either[E, B]): Either[E, Iterable[B]] = {
       val buf = List.newBuilder[B]
       val it  = list.iterator
       while (it.hasNext)
