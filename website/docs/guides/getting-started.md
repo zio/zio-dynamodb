fe5a88f2f0c74d42bce92594d4545c5ffe2b94ae
khajavi <235974+khajavi@users.noreply.github.com>
2026-08-20 19:34:27 +0000

    Update README.md

---
+++ website/docs/guides/getting-started.md
@@ -0,0 +1,47 @@
+---
+id: getting-started
+title: "Getting Started"
+---
+
+## Add the dependency to your build.sbt file
+
+```scala
+libraryDependencies += "dev.zio" %% "zio-dynamodb" % 1.0.0-RC27
+```
+
+### Read & write data to/from DynamoDB
+
+```scala
+import zio.aws.core.config
+import zio.aws.{ dynamodb, netty }
+import zio.dynamodb.DynamoDBQuery.{ get, put }
+import zio.dynamodb.{ DynamoDBExecutor }
+import zio.schema.{ DeriveSchema, Schema }
+import zio.ZIOAppDefault
+import zio.dynamodb.ProjectionExpression
+
+object Main extends ZIOAppDefault {
+
+  final case class Person(id: Int, firstName: String)
+  object Person {
+    implicit lazy val schema: Schema.CaseClass2[Int, String, Person] = DeriveSchema.gen[Person]
+
+    val (id, firstName) = ProjectionExpression.accessors[Person]
+  }
+  val examplePerson = Person(1, "avi")
+
+  private val program = for {
+    _      <- put("personTable", examplePerson).execute
+    person <- get("personTable")(Person.id.partitionKey === 1).execute
+    _      <- zio.Console.printLine(s"hello $person")
+  } yield ()
+
+  override def run =
+    program.provide(
+      netty.NettyHttpClient.default,
+      config.AwsConfig.default, // uses real AWS dynamodb
+      dynamodb.DynamoDb.live,
+      DynamoDBExecutor.live
+    )
+}
+```
---
+++ website/docs/sidebars.js
@@ -0,0 +1,107 @@
+const sidebars = {
+  sidebar: [
+    {
+      type: "category",
+      label: "ZIO DynamoDB",
+      collapsed: false,
+      link: { type: "doc", id: "index" },
+      items: [
+        // Concepts
+        {
+          type: "category",
+          collapsed: false,
+          label: "Concepts",
+          items: [
+            "concepts/architecture",
+            "concepts/high-level-api",
+            "concepts/low-level-api",
+          ],
+
+        },
+        // Guides
+        {
+          type: "category",
+          collapsed: false,
+          label: "Guides",
+          items: [
+            "guides/getting-started",
+            "guides/cheat-sheet",
+            "guides/data-modelling",
+            "guides/codec-customization",
+            "guides/transactions",
+            "guides/testing",
+            "guides/ce-interop",
+            "guides/future-interop",
+          ]
+        },
+        // Reference
+        {
+          type: "category",
+          collapsed: false,
+          label: "Reference",
+          items: [
+            {
+              type: "category",
+              label: "High Level API",
+              collapsed: true,
+              link: { type: "doc", id: "reference/hi-level-api/index" },
+              items: [
+                {
+                  type: "category",
+                  label: "Creating Models",
+                  collapsed: true,
+                  link: { type: "doc", id: "reference/hi-level-api/creating-models/index" },
+                  items: [
+                    "reference/hi-level-api/creating-models/field-traversal",
+                  ]
+                },
+                {
+                  type: "category",
+                  label: "CRUD Operations",
+                  collapsed: true,
+                  link: { type: "doc", id: "reference/hi-level-api/crud-operations/index" },
+                  items: [
+                    "reference/hi-level-api/crud-operations/put",
+                    "reference/hi-level-api/crud-operations/get",
+                    "reference/hi-level-api/crud-operations/update",
+                    "reference/hi-level-api/crud-operations/delete",
+                  ]
+                },
+                {
+                  type: "category",
+                  label: "Scan and Query Operations",
+                  collapsed: true,
+                  link: { type: "doc", id: "reference/hi-level-api/scan-and-query-operations/index" },
+                  items: [
+                    "reference/hi-level-api/scan-and-query-operations/scan-all",
+                    "reference/hi-level-api/scan-and-query-operations/scan-some",
+                    "reference/hi-level-api/scan-and-query-operations/query-all",
+                    "reference/hi-level-api/scan-and-query-operations/query-some",
+                  ]
+                },
+                "reference/hi-level-api/primary-keys",
+              ]
+            },
+            {
+              type: "category",
+              label: "Low Level API",
+              collapsed: false,
+              link: { type: "doc", id: "reference/low-level-api/index" },
+              items: [
+                "reference/low-level-api/attribute-value",
+                "reference/low-level-api/dollar-function",
+              ]
+            },
+            "reference/dynamodb-query",
+            "reference/projection-expression",
+            "reference/error-handling",
+            "reference/auto-batching-and-parallelisation",
+            "reference/zio-dynamodb-json",
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+module.exports = sidebars;
